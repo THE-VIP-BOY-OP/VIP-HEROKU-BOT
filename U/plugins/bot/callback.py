ec4be0e1c01a9ff32ee71ea507bd838eaa98d045
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 18:35:15 +0530

    Update bot

---
+++ U/plugins/bot/callback.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery
+
+from config import BANNED_USERS
+
+@Client.on_callback_query(filters.regex("close") & ~BANNED_USERS)
+async def close_menu(_, query: CallbackQuery):
+    try:
+        await query.answer()
+        await query.message.delete()
+    except:
+        pass
