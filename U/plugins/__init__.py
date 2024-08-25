fde877383d0d75f8341109bde469f5075cbda4ae
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 18:23:17 +0530

    Update __init__.py

--- U/plugins/__init__.py
+++ U/plugins/__init__.py
@@ -1,17 +1,19 @@
 import glob
-from os.path import dirname, isfile, join
+from os.path import dirname, isfile
+
 
 def __list_all_modules():
     work_dir = dirname(__file__)
-    mod_paths = glob.glob(join(work_dir, "*/*.py"))
+    mod_paths = glob.glob(work_dir + "/*/*.py")
 
     all_modules = [
-        (f.replace(join(work_dir, ""), "").replace(".py", "").replace("/", ".").lstrip('.'))
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
         for f in mod_paths
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
 
     return all_modules
+
 
 ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
