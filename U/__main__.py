6bf861832ae36f5b3525ce0765c10800561325a7
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 18:23:45 +0530

    Update __main__.py

--- U/__main__.py
+++ U/__main__.py
@@ -19,16 +19,16 @@ async def main():
     log.info(f"Bot Started as {app.me.first_name}")
 
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module(all_module)
+        imported_module = importlib.import_module("U.plugins" + all_module)
 
-        """if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 if imported_module.__MODULE__.lower() not in HELPABLE:
                     HELPABLE[imported_module.__MODULE__.lower()] = imported_module
                 else:
                     raise Exception(
                         f"Can't have two modules with name! '{imported_module.__MODULE__}' Please Change One"
-                    )"""
+                    )
 
     try:
         await app.send_message(LOG_GROUP_ID, "Started")
