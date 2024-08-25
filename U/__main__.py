8cf2e1c23d5f4e972bd28fb85c571f2f79cd4770
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 12:34:58 +0530

    Update __main__.py

--- U/__main__.py
+++ U/__main__.py
@@ -9,13 +9,18 @@ from bot import app
 from .logging import LOGGER
 log = LOGGER("U")
 
+from config import LOG_GROUP_ID
+
 async def main():
     
     log.info("Starting bot....")
 
     await app.start()
     log.info(f"bot Started as {app.me.first_name}")
-
+    try:
+        await app.send_message(LOG_GROUP_ID, "Started"
+    except:
+        pass
     await idle()
     await app.stop()
     log.info("Stopping bot Good Bye")
