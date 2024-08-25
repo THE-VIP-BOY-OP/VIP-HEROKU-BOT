0c6c4f41d7e63043c08fcb15bc064c7753b613a9
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 12:01:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,13 @@
-import asyncio
-
-import uvloop
-uvloop.install()
-
-import logging
-from pyrogram import Client, idle
+from pyrogram import Client
 
 from config import API_ID, API_HASH, BOT_TOKEN
 
-log = logging.getLogger(__name__)
-
-async def main():
-
-    app = Client(
-        "Boss",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        bot_token=BOT_TOKEN,
-        in_memory=True,
-        plugins=dict(root="plugins"),
-        max_concurrent_transmissions=9,     
-    )
-
-    log.info("Starting bot")
-
-    await app.start()
-    await idle()
-    await app.stop()
-
-if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+app = Client(
+    "Boss",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    in_memory=True,
+    plugins=dict(root="plugins"),
+    max_concurrent_transmissions=9,     
+)
