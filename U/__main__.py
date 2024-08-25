3dd18eb5dd09d340f38c5d4861433e2c32c2310e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 18:08:51 +0530

    Update __main__.py

--- U/__main__.py
+++ U/__main__.py
@@ -18,10 +18,10 @@ async def main():
     await app.start()
     log.info(f"Bot Started as {app.me.first_name}")
 
-    """for all_module in ALL_MODULES:
+    for all_module in ALL_MODULES:
         imported_module = importlib.import_module(all_module)
 
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+        """if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 if imported_module.__MODULE__.lower() not in HELPABLE:
                     HELPABLE[imported_module.__MODULE__.lower()] = imported_module
