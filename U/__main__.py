70bf2543153b41e9bf3367c32770002f6a3a982f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 12:09:35 +0530

    Update __main__.py

--- U/__main__.py
+++ U/__main__.py
@@ -6,10 +6,19 @@ uvloop.install()
 from pyrogram import idle
 from bot import app
 
+from .logging import LOGGER
+log = LOGGER("U")
+
 async def main():
+    
+    log.info("Starting bot....")
+
     await app.start()
+    log.info(f"bot Started as {app.me.first_name}"
+
     await idle()
     await app.stop()
+    log.info("Stopping bot Good Bye")
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
