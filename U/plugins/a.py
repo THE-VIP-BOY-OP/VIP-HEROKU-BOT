93325e5712745bdcbcc7e201ace6ee543b7fccb3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 12:36:10 +0530

    Update a.py

--- U/plugins/a.py
+++ U/plugins/a.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters 
 
-@Client.on_message(filters.private & filters.text)
+@Client.on_message(filters.private & filters.command("start"))
 async def start(c,m):
     await m.reply_text(c.me.username)
