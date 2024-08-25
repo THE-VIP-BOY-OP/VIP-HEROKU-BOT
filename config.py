0b4c9d08ac18731a5656933ab3c6ec4a5b9c2200
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 11:43:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 from os import getenv
 
-API_ID = getenv("API_ID", None)
+from dotenv import load_dotenv
+
+API_ID = int(getenv("API_ID", 25488022))
+API_HASH = getenv("API_HASH", "0c999a454fddd79251213be7944811e8")
+BOT_TOKEN = getenv("BOT_TOKEN", "6751747123:AAH4631aqVNXv01XufsLFQgWLLkgtU_Du-E")
