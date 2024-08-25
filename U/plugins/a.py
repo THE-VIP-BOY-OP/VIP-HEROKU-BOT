a557fd41380f81ea8e9ce3f71d82d780e4f26b35
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 12:45:30 +0530

    Update a.py

--- U/plugins/a.py
+++ U/plugins/a.py
@@ -2,4 +2,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(c,m):
-    await m.reply_text("Radhe Radhe\n\nBot is now Under Development")
+    await m.reply_text("Radhe Radhe\nBot is now Under Development")
