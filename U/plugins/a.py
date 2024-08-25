9e197a1ea41c5cb27bcdd1bc9814d8338918eca5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-25 18:06:45 +0530

    Delete U/plugins/a.py

--- U/plugins/a.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters 
-from bot import app
-
-@app.on_message(filters.private & filters.command("start"))
-async def start(c,m):
-    await m.reply_text("Radhe Radhe\nBot is now Under Development")
