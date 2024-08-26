b7e5ffacc46144735396c9c65f4874566fecefb0
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-26 11:32:34 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,10 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery
 
-from config import BANNED_USERS
 
-
-@Client.on_callback_query(filters.regex("close") & ~BANNED_USERS)
+@Client.on_callback_query(filters.regex("close"))
 async def close_menu(_, query: CallbackQuery):
     try:
         await query.answer()
