34cd9981fc706a00533853a7b6321a39b6180bb3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-26 19:59:02 +0530

    Update vcmembers.py

--- plugins/vcmembers.py
+++ plugins/vcmembers.py
@@ -9,13 +9,15 @@ from utils import filters
 
 @Client.on_message(filters.command("vcmembers") & filters.sudo)
 async def vc_members(client, message):
-   msg = await message.reply_text("Please wait...")
+    msg = await message.reply_text("Please wait...")
 
     full_chat: base.messages.ChatFull = await client.invoke(
         GetFullChannel(channel=(await client.resolve_peer(message.chat.id)))
     )
+
     if not full_chat.full_chat.call:
-        return await msg.edit("Radhe Radhe\nOops looks like Voice chat is off")
+        return await msg.edit("Radhe Radhe\nOops, it looks like Voice chat is off")
+
     access_hash = full_chat.full_chat.call.access_hash
     ids = full_chat.full_chat.call.id
     input_group_call = InputGroupCall(id=ids, access_hash=access_hash)
@@ -23,7 +25,7 @@ async def vc_members(client, message):
     result = await client.invoke(GetGroupCall(call=input_group_call, limit=1))
 
     users = result.participants
- 
+
     if not users:
         await msg.edit("There are no members in the voice chat currently.")
         return
@@ -44,16 +46,16 @@ async def vc_members(client, message):
                         username = "Can't Say"
         else:
             user_id = user.peer.user_id
-           try:
+            try:
                 user_info = await client.get_users(user_id)
                 title = user_info.mention
                 username = user_info.username or "No Username"
-           except:
-               user_info = result.users
-              for user in user_info:
-                 if user.id == user_id:
-                    username = user.username or "No Username"
-                    title = [user.first_name](f"tg://user?id={user_id}")
+            except:
+                for user_obj in result.users:
+                    if user_obj.id == user_id:
+                        username = user_obj.username or "No Username"
+                        title = f"[{user_obj.first_name}](tg://user?id={user_id})"
+
         is_left = user.left
         just_joined = user.just_joined
         is_muted = True if user.muted and not user.can_self_unmute else False
