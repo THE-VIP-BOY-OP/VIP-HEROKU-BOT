5126f57c7721a4025a5774f57db99468317a2bae
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-26 18:02:47 +0530

    Update vcmembers.py

--- plugins/vcmembers.py
+++ plugins/vcmembers.py
@@ -10,48 +10,54 @@ from utils import filters
 @Client.on_message(filters.command("vcmembers") & filters.sudo)
 async def vc_members(client, message):
     full_chat: base.messages.ChatFull = await client.invoke(
-            GetFullChannel(
-                channel=(
-                    await client.resolve_peer(message.chat.id)
-                )
-            )
+        GetFullChannel(
+            channel=(await client.resolve_peer(message.chat.id))
+        )
     )
-        
 
     access_hash = full_chat.full_chat.call.access_hash
     ids = full_chat.full_chat.call.id
     input_group_call = InputGroupCall(id=ids, access_hash=access_hash)
-        
 
-    result = await client.invoke(GetGroupCall(
+    result = await client.invoke(
+        GetGroupCall(
             call=input_group_call,
             limit=1
-        ))
-        
-    users =  result.participants
-    msg = await message.reply_text("please wait")
+        )
+    )
+
+    users = result.participants
+    msg = await message.reply_text("Please wait...")
+
     if not users:
-        await msg.edit("There Are no memebers in voice chat currently")
+        await msg.edit("There are no members in the voice chat currently.")
+        return
+
     mg = ""
     for user in users:
-    	if user.channel_id:
-    	    user_id = -100 + user.channel_id
-    	    title = (await client.get_chat(user_id)).title
-            username = (await client.get_chat(user_id)).username or "Private Group"
-        if user.peer:
+        if user.channel_id:
+            user_id = -100 + user.channel_id
+            chat = await client.get_chat(user_id)
+            title = chat.title
+            username = chat.username or "Private Group"
+        else:
             user_id = user.peer.user_id
-            name = (await client.get_users(user_id)).mention or "Unknown"
-            username = (await client.get_users(user_id)).mention or "No User Name"
+            user_info = await client.get_users(user_id)
+            title = user_info.mention
+            username = user_info.username or "No Username"
+
         is_left = user.left
-        just_joined= user.just_joined
+        just_joined = user.just_joined
         is_muted = True if user.muted and not user.can_self_unmute else False
-        mg += f""" {'Title' if  user.channel_id else 'Name'} = {title if user.channel_id else name}
-            id= {user_id}
-            username = {username}
-            is lefted = {is_left}
-            is_just_joined = {just_joined}
-            is_muted = {is_muted}"""
-            
-    if not mg == ""
+
+        mg += f""" {'Title' if user.channel_id else 'Name'} = {title}
+        ID = {user_id}
+        Username = {username}
+        Is Left = {is_left}
+        Just Joined = {just_joined}
+        Is Muted = {is_muted}\n\n"""
+
+    if mg:
         await msg.edit(mg)
-        
+    else:
+        await msg.edit("No members found.")
