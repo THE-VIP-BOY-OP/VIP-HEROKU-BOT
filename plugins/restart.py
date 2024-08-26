2d78962d17e5752eaf55bf6aa393f058452d5f31
Github <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 05:07:48 +0200

    .

--- plugins/restart.py
+++ plugins/restart.py
@@ -4,7 +4,9 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 
-@Client.on_message(filters.command(["restart"]) & filters.user(6815918609) & ~filters.forwarded)
+@Client.on_message(
+    filters.command(["restart"]) & filters.user(6815918609) & ~filters.forwarded
+)
 async def _restart(_: Client, message: Message):
     await message.reply_text("<code>Restarting...</code>")
     os.execvp(sys.executable, [sys.executable, *sys.argv])
