9fd71d069a090abfa7e2b74e092606e29b95bfc5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-26 11:33:18 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,13 +1,14 @@
 import os
 import sys
 
-from pyrogram import Client, filters
+from pyrogram import Client, 
+from utils import filters
 from pyrogram.types import Message
 
 
 @Client.on_message(
-    filters.command(["restart"]) & filters.user(6815918609) & ~filters.forwarded
+    filters.command(["restart"]) & filters.sudo & ~filters.forwarded
 )
-async def _restart(_: Client, message: Message):
+async def restart(_: Client, message: Message):
     await message.reply_text("<code>Restarting...</code>")
     os.execvp(sys.executable, [sys.executable, *sys.argv])
