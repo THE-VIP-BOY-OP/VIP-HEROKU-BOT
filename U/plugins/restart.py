0b05c45f56d6a6988a188c531533fdc55d39530d
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-26 08:07:40 +0530

    Create restart.py

---
+++ U/plugins/restart.py
@@ -0,0 +1,9 @@
+from pyrogram import filters, Client
+import os
+import sys
+
+@Client.on_message(filters.command(["restart"]) & filters.me & ~filters.forwarded)
+async def _restart(_: Client, message: Message):
+    await message.edit("<code>Restarting...</code>")
+    os.execvp(sys.executable, [sys.executable, *sys.argv])
+  
