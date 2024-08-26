7f7572dfb6bfbd2294b4841ba77461ff605053c7
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-26 09:03:12 +0530

    Update genstring.py

--- genstring.py
+++ genstring.py
@@ -6,13 +6,13 @@ API_ID = input("\nEnter Your API_ID:\n > ")
 API_HASH = input("\nEnter Your API_HASH:\n > ")
 
 i = Client(
-    "boas", 
+    "Cute", 
     in_memory=True, 
     api_id=API_ID, 
     api_hash=API_HASH,
-    app_version="Boss 2.1.25",
+    app_version="Cute V2.1.25",
     lang_pack="android",
-    device_model="Boss",
+    device_model="Cute",
 )
 
 
