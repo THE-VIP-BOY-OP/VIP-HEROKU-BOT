f282ab63e541ebb94629faa6a72998c951efe047
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-26 12:29:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,5 @@ async def main():
 
 
 if __name__ == "__main__":
-    with asyncio.Runner() as runner:
-        loop = runner.get_loop()
-        loop.run_until_complete(main())
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
