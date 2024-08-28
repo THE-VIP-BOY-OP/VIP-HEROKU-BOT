d4751722aca850817a430600b8e0c5de35623e32
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-28 06:54:41 +0530

    Update vcmembers.py

--- plugins/vcmembers.py
+++ plugins/vcmembers.py
@@ -5,7 +5,7 @@ from utils import filters
 
 @Client.on_message(filters.command("vcmembers") & filters.sudo)
 async def vc_members(client, message):
-    msg = await message.reply_text("**Radhe Radhe**\nPlease wait...")
+    msg = await message.reply_text("Fetching Participants please wait")
     try:
         async for m in client.get_call_members(message.chat.id):
             chat_id = m.chat.id
@@ -24,10 +24,10 @@ async def vc_members(client, message):
                     title = (await client.get_users(chat_id)).mention
                 except:
                     title = m.chat.first_name
-        MS = 'MUTED' if is_muted else 'SPEAKING'
+        MS = '**Muted**' if is_muted else '**Speaking**'
         MSK = is_muted if is_muted else is_speaking
-        TEXT = "**NAME: {0}**\n    USERNAME: {1}\n    VIDEO SHARING: {2}\n    SCREEN SHARING: {3}\n    IS_HAND_RAISED: {4}\n    {5}: {6}\n     LEFT THE GROUP: {7}".format(title, username, is_video_enabled, is_screen_sharing_enabled, is_hand_raised, MS, MSK, is_left)
-        await msg.edit(TEXT or "No members found.")
+        TEXT = "**NAME: {0}**\n    **Username:** {1}\n    **Video Sharing:** {2}\n    **Screen Sharing:** {3}\n    **Is_hand_raised**: {4}\n    {5}: {6}\n    **Left The Group:** {7}".format(title, username, is_video_enabled, is_screen_sharing_enabled, is_hand_raised, MS, MSK, is_left)
+        await msg.edit(TEXT or "No Participants in Voice Chat")
     except ValueError as e:
         await msg.edit(str(e))
 
