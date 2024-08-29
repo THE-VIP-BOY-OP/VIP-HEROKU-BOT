1186f36fcdab32ee5a557cd721375b9c48f33cf2
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-29 18:48:31 +0530

    Update handlers.py

--- utils/handlers.py
+++ utils/handlers.py
@@ -1,16 +1,5 @@
-class HandlerManager:
-    def __init__(self):
-        self.handler_list = {}
+from pyrogram import Client
 
-    def add_to_handler_list(self, commands, handler_func):
-        for command in commands:
-            self.handler_list[command] = handler_func
-
-    def get_handler(self, command):
-        return self.handler_list.get(command)
-
-    def get_all_handlers(self):
-        handler_info = []
-        for command, func in self.handler_list.items():
-            handler_info.append(f"Command: {command} - Function: {func.__name__}")
-        return "\n".join(handler_info)
+class VClient(Client):
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
