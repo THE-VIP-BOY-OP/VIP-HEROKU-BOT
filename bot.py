b4d5da247d44bb77b1cf6926c8f57402a3550461
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-31 14:39:21 +0530

    Update

---
+++ bot.py
@@ -0,0 +1,13 @@
+from config import API_HASH, API_ID, STRING_SESSION
+from utils import Client
+
+app = Client(
+    "Boss", 
+    api_id=API_ID,
+    api_hash=API_HASH,
+    app_version=f"Cute {v}",
+    session_string=STRING_SESSION,
+    in_memory=True,
+    plugins=dict(root="plugins"),
+    max_concurrent_transmissions=9,
+)
