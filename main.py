443691e87320f6258b61b480d73326c5ff17ace3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-31 15:02:49 +0530

    Update main.py1

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import platform
 
 from pyrogram import idle
 
-from bot import app
+from pyrogram import __version__ as v
+import uvloop
+from config import API_HASH, API_ID, STRING_SESSION
+from utils import Client
+
+uvloop.install()
+
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -24,6 +31,16 @@ log = logging.getLogger("U")
 
 async def main():
     log.info("Starting Userbot...")
+    app = Client(
+        "Boss", 
+        api_id=API_ID,
+        api_hash=API_HASH,
+        app_version=f"Cute {v}",
+        session_string=STRING_SESSION,
+        in_memory=True,
+        plugins=dict(root="plugins"),
+        max_concurrent_transmissions=9,
+    )
     await app.start()
     log.info("Userbot started")
     await idle()
