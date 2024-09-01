4d036a1d0c28514874d1ae7568ebadc7acb9d0b0
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 16:43:11 +0530

    Update clients.py

--- Vivek/core/clients.py
+++ Vivek/core/clients.py
@@ -20,7 +20,7 @@ class Vivek(VClient):
             name="Vivek", 
             api_id=API_ID,
             api_hash=API_HASH,
-            app_version=f"Cute {v}",
+            #app_version=f"Cute {v}",
             session_string=STRING_SESSION,
             #in_memory=True,
             #plugins=dict(root="Vivek/plugins"),
