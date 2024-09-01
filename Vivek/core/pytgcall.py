11da06aa3ecb458f4197220912b209ff3390f2d4
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 13:34:09 +0530

    Create pytgcall.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

---
+++ Vivek/core/pytgcall.py
@@ -0,0 +1,15 @@
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+
+from config import API_ID, API_HASH, STRING_SESSION
+
+class pytgcall(PyTgCalls):
+    def __init__(self):
+        self.app = Client(
+            "pytgcall",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            session_string=STRING_SESSION,
+        )
+        
+        super().__init__(self.app, cache_duration=100)
