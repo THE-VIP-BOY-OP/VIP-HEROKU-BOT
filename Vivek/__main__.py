8c2de5a0135201ba217b122f9e910896d4e3a642
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 12:33:55 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -1,18 +1,13 @@
 import asyncio
-import platform
 from pyrogram import idle
-from Vivek import app, LOGGER
+from Vivek import app
 
-log = LOGGER("Vivek")
 
 async def main():
 
     await app.start()
-    log.info("Userbot started")
     await idle()
     await app.stop()
-    log.info("Stopping bot Good Bye")
-
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop_policy().get_event_loop()
