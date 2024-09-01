d207af6ec3d1e606cc8049f185c6c1f6e925050a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 17:57:50 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -1,18 +1,18 @@
 import asyncio
 
 from pyrogram import idle
-from Vivek import app
-
-from Vivek.core.pytgcall import call
+from Vivek import app, bot, call
 
 async def main():
 
     await app.start()
-    await app.import_all_module()
+    await bot.start()
     await call.start()
+
     await idle()
 
     await app.stop()
+    await bot.stop()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop_policy().get_event_loop()
