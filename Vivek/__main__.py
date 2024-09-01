bcfa57ad41e03d424a4c7dc1cad1e017a8bac6d5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 18:11:50 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -5,17 +5,17 @@ from Vivek import app, bot, call, HELPABLE, LOGGER
 from Vivek.plugins import ALL_MODULES
 
 async def main():
-    LOGGER(__name__).info("Staring Clients")
+    LOGGER("Vivek").info("Staring Clients")
 
     await app.start()
-    await bot.start()
+    #await bot.start()
     for all_module in ALL_MODULES:
         imported_module = importlib.import_module("Vivek.plugins" + all_module)
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
     await call.start()
-    LOGGER(__name__).info("Clients Started Successfully")
+    LOGGER("Vivek").info("Clients Started Successfully")
     await idle()
 
     await app.stop()
