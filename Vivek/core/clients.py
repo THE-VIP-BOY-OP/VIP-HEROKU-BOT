cd98246320104677df0e1c12ab84e6d448999b38
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 17:15:55 +0530

    Update clients.py

--- Vivek/core/clients.py
+++ Vivek/core/clients.py
@@ -4,7 +4,7 @@ uvloop.install()
 import importlib
 
 from pyrogram import __version__ as v
-from Vivek.utils import VClient
+from Vivek.utils import Client as VClient
 from Vivek.logging import LOGGER
 
 from config import API_HASH, API_ID, STRING_SESSION, BOT_TOKEN, LOG_GROUP_ID
