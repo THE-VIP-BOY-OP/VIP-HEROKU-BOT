1b3721b3156e926f935b74f451631963da3b42d2
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 10:17:08 +0530

    Update clients.py

--- Vivek/core/clients.py
+++ Vivek/core/clients.py
@@ -22,13 +22,13 @@ class Vivek(Vivek):
         )
 
     self.bot = Client(
-                    "Vivek1",
-                    api_id=API_ID,
-                    api_hash=API_HASH,
-                    app_version=f"Cute {v}",
-                    bot_token=BOT_TOKEN,
-                    in_memory=True,
-                    plugins=dict(root="Vivek/plugins/bot"),
-                    max_concurrent_transmissions=9
-                )
+                "Vivek1",
+                api_id=API_ID,
+                api_hash=API_HASH,
+                app_version=f"Cute {v}",
+                bot_token=BOT_TOKEN,
+                in_memory=True,
+                plugins=dict(root="Vivek/plugins/bot"),
+                max_concurrent_transmissions=9
+            )
            
