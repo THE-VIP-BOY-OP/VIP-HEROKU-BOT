8a09d077000dfd650840c26e8ed5c24526404319
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 12:40:14 +0530

    Update eval.py

--- Vivek/plugins/eval.py
+++ Vivek/plugins/eval.py
@@ -10,7 +10,7 @@ from time import time
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-
+from Vivek import app 
 
 async def aexec(code, client, message):
     exec(
