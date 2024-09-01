30964a49bb0e206d3cefae6b6653d831b87d5786
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 10:16:34 +0530

    Update clients.py

--- Vivek/core/clients.py
+++ Vivek/core/clients.py
@@ -1,14 +1,13 @@
 from pyrogram import __version__ as v
-from config import API_HASH, API_ID, STRING_SESSION
-import platform
-import asyncio
-
-from utils import Client
+from utils import Vivek
 import uvloop
 
 uvloop.install()
 
-class Vivek(Client):
+from config import API_HASH, API_ID, STRING_SESSION, BOT_TOKEN
+
+
+class Vivek(Vivek):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
@@ -30,5 +29,6 @@ class Vivek(Client):
                     bot_token=BOT_TOKEN,
                     in_memory=True,
                     plugins=dict(root="Vivek/plugins/bot"),
-                    max_concurrent_transmissions=9,)
+                    max_concurrent_transmissions=9
+                )
            
