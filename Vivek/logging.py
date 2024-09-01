5dc726390787905f79d283bc44cdd053c287b365
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 18:33:00 +0530

    Update logging.py

--- Vivek/logging.py
+++ Vivek/logging.py
@@ -1,15 +1,19 @@
 from loguru import logger
 import sys
+import time
 
 logger.remove()
 
-logger.add(
-    "log.txt",
-    rotation="5 MB",
-    retention="10 files",
-    compression="zip",
-    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {name} - {message}",
-)
+def clt(seconds: int):
+    return f"{seconds} seconds"
+
+def setup_logger(exp: int):
+    logger.add(
+        "log.txt",
+        rotation="5 MB", retention=clt(exp),
+        compression="zip",
+        format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {name} - {message}",
+    )
 
 logger.add(
     sys.stdout,
@@ -19,11 +23,11 @@ logger.add(
            "<cyan>{name}</cyan> - <level>{message}</level>",
 )
 
-
 logger.level("ERROR", color="<red>")
 logger.disable("pyrogram")
 logger.disable("httpx")
 
+def LOGGER(name: str, exp: int = 864000):
 
-def LOGGER(name: str):
+    setup_logger(exp)
     return logger.bind(name=name)
