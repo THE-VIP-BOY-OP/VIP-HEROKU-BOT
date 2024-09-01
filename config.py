c84165033fc18081a46486774ca0e51a4382478f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 10:45:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", 25488022))
 API_HASH = getenv("API_HASH", "0c999a454fddd79251213be7944811e8")
 STRING_SESSION = getenv("STRING_SESSION", None)
-BOT_TOKEN = grtenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 LOG_GROUP_ID = getenv("LOG_GROUP_ID", -1002042572827)
 OWNER_ID = list(
     map(int, getenv("OWNER_ID", "6815918609 7301077117 7428672286").split())
