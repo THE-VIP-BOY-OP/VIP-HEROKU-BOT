9663f8642f815160176cda9ed264a0a39c1503ea
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 18:10:07 +0530

    Update common_chats.py

--- Vivek/plugins/tools/common_chats.py
+++ Vivek/plugins/tools/common_chats.py
@@ -5,30 +5,29 @@ from pyrogram.errors import UsernameInvalid
 @app.on_message(filters.sudo & filters.command(["common_chats"]))
 async def get_common_chats(client, message):
 
-    if len(message.command) < 2 or not message.reply_to_message:
-        return await message.reply_text("Usage: /common_chats @username")
-    if message.reply_to_message:
-        username = message.reply_to_message.from_user.username if message.reply_to_message.from_user.username else message.reply_to_message.from_user.id
+    if len(message.command) < 2 and not message.reply_to_message:
+        return await message.reply_text("Usage: /common_chats @username or reply to a user.")
 
-    if not message.reply_to_message:
+    if message.reply_to_message:
+        username = message.reply_to_message.from_user.username or message.reply_to_message.from_user.id
+    else:
         username = message.command[1]
 
     try:
-        a = await app.get_common_chats(username)
+        common_chats = await app.get_common_chats(username)
     except UsernameInvalid:
         return await message.reply_text("The username is invalid. Please provide a valid username.")
-    
-    if not a:
+
+    if not common_chats:
         return await message.reply_text("No common chats found.")
-    
-    msg = "Found Some Common Chats:\n"
-    for chat in a:
-        title = chat.title
+
+    msg = "Found Some Common Chats:\n\n"
+    for chat in common_chats:
+        title = chat.title or "Untitled Chat"
         chat_id = chat.id
         username = chat.username or "Private Group"
         members_count = chat.members_count or "Unknown"
 
         msg += f"Chat Name: {title}\n    ID: {chat_id}\n    Username: {username}\n    Members: {members_count}\n\n"
-        
-    if msg != "Found Some Common Chats:\n":
-        await message.reply_text(msg)
+
+    await message.reply_text(msg)
