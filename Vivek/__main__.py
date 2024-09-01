1b8fdb6d962331040aae8ac8b99ffc7fe49dfd27
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 09:10:38 +0530

    Update and rename main.py to Vivek/__main__.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -1,17 +1,7 @@
 import asyncio
 import logging
 import platform
-
 from pyrogram import idle
-
-from pyrogram import __version__ as v
-import uvloop
-from config import API_HASH, API_ID, STRING_SESSION
-from utils import Client
-
-uvloop.install()
-
-
 
 logging.basicConfig(
     level=logging.INFO,
@@ -31,16 +21,6 @@ log = logging.getLogger("U")
 
 async def main():
     log.info("Starting Userbot...")
-    app = Client(
-        "Boss", 
-        api_id=API_ID,
-        api_hash=API_HASH,
-        app_version=f"Cute {v}",
-        session_string=STRING_SESSION,
-        in_memory=True,
-        plugins=dict(root="plugins"),
-        max_concurrent_transmissions=9,
-    )
     await app.start()
     log.info("Userbot started")
     await idle()
