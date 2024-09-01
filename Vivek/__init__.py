104100be13e0d83842998a93f19583f6eb9e8ab6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 17:56:53 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -1,7 +1,47 @@
-from Vivek.core.clients import Vivek
+import uvloop
+uvloop.install()
+
+
 from .logging import LOGGER
 from Vivek.utils.filters import edit_filters
 
-app = Vivek()
+from pyrogram import __version__ as v
+from pyrogram import Client
+
+from config import API_HASH, API_ID, STRING_SESSION, BOT_TOKEN, LOG_GROUP_ID
+from pytgcalls import PyTgCalls
+
+from Vivek.utils import VClient
+HELPABLE = {}
+
+app = VClient(
+    name="Vivek", 
+    api_id=API_ID,
+    api_hash=API_HASH,
+    app_version=f"Cute {v}",
+    session_string=STRING_SESSION,
+    in_memory=True,
+    plugins=dict(root="Vivek/plugins"),
+)
+
+bot = VClient(
+    name="Vivek1",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    app_version=f"Cute {v}",
+    bot_token=BOT_TOKEN,
+    in_memory=True,
+    plugins=dict(root="Vivek/plugins/bot"),
+    max_concurrent_transmissions=9
+)
+
+call = PyTgCalls(
+    Client(
+        name="Vivek", 
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION,
+    )
+)
 
 edit_filters()
