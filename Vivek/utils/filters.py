b2b8655839b0136578b84b5974b3d0eefeb8bb85
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 13:36:37 +0530

    Update filters.py

--- Vivek/utils/filters.py
+++ Vivek/utils/filters.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message
 from config import OWNER_ID, PREFIX
 
 
-def edit_filters():
+def filters():
 
     filters.reaction = filters.create(
         lambda _, __, m: bool(m.reactions), "ReactionFilter"
