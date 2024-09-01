5b478ea136ba4e7feffeb3c77a44e99969cc165e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 13:36:22 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -1,8 +1,9 @@
 from Vivek.core.clients import Vivek
 from Vivek.core.pytgcall import pytgcall
 from .logging import LOGGER
-from Vivek.utils.filters import edit_filters
+from Vivek.utils.filters import filters
 
 app = Vivek()
 call = pytgcall()
-edit_filters()
+
+filters()
