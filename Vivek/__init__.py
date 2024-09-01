f2030baae62e5ad691eef98ed69faf9a264b6964
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 22:45:01 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -2,7 +2,7 @@ import uvloop
 
 uvloop.install()
 
-from Vivek.core.clients import app, bot, call
+from Vivek.core.clients import app, bot
 from Vivek.utils.filters import edit_filters
 
 from .logging import LOGGER
