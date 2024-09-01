4f1165eede6dab713a2bd9baee622a1a383b6c7d
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 17:59:09 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -1,12 +1,18 @@
 import asyncio
 
 from pyrogram import idle
-from Vivek import app, bot, call
+from Vivek import app, bot, call, HELPABLE
+from Vivek.plugins import ALL_MODULES
 
 async def main():
 
     await app.start()
     await bot.start()
+    for all_module in ALL_MODULES:
+        imported_module = importlib.import_module("Vivek.plugins" + all_module)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
     await call.start()
 
     await idle()
