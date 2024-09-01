91423641d46dba8874e4a98af900abe0de3e0bed
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 17:13:35 +0530

    Update pytgcall.py

--- Vivek/core/pytgcall.py
+++ Vivek/core/pytgcall.py
@@ -1,14 +1,13 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from Vivek.utils import VClient
 from config import API_ID, API_HASH, STRING_SESSION
 
 
-app = VClient(
-            name="Vivek1",
-            api_id=API_ID,
-            api_hash=API_HASH, 
-            session_string=STRING_SESSION,
+app = Client(
+    name="Vivek1",
+    api_id=API_ID,
+    api_hash=API_HASH, 
+    session_string=STRING_SESSION,
         )
 
 
