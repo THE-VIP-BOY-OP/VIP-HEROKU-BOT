e472e38bb49fba9ac55e81acae7c963bbc3311d3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 18:00:35 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -1,10 +1,11 @@
 import asyncio
 
 from pyrogram import idle
-from Vivek import app, bot, call, HELPABLE
+from Vivek import app, bot, call, HELPABLE, LOGGER
 from Vivek.plugins import ALL_MODULES
 
 async def main():
+    LOGGER(__name__}).info("Staring Clients")
 
     await app.start()
     await bot.start()
@@ -14,7 +15,7 @@ async def main():
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
     await call.start()
-
+    LOGGER(__name__).info("Clients Started Successfully")
     await idle()
 
     await app.stop()
