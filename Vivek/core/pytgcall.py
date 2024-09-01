eb25465ae3ce760b2cc8044326855496e9993baf
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-01 16:48:18 +0530

    Update pytgcall.py

--- Vivek/core/pytgcall.py
+++ Vivek/core/pytgcall.py
@@ -5,18 +5,4 @@ from config import API_ID, API_HASH, STRING_SESSION
 
 from .clients import app
 
-
-"""class pytgcall(PyTgCalls):
-    def __init__(self):
-        super().__init__(
-            Client(
-                name="pytgcall",
-                api_id=API_ID,
-                api_hash=API_HASH,
-                session_string=STRING_SESSION
-            )
-        )
-
-call = pytgcall()"""
-
 call = PyTgCalls(app)
