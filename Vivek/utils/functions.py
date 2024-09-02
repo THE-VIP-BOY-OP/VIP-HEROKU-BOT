09dc04ae041b5cb9cbf0fad5b7a95f3777b4c03b
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 09:49:58 +0530

    Update functions.py

--- Vivek/utils/functions.py
+++ Vivek/utils/functions.py
@@ -1,6 +1,8 @@
 from pyrogram import Client
 
 pause = {}
+mute = {}
+active = []
 
 
 class VClient(Client):
@@ -21,3 +23,38 @@ async def music_on(chat_id: int):
 
 async def music_off(chat_id: int):
     pause[chat_id] = False
+
+async def is_muted(chat_id: int) -> bool:
+    mode = mute.get(chat_id)
+    if not mode:
+        return False
+    return mode
+
+
+async def mute_on(chat_id: int):
+    mute[chat_id] = True
+
+
+async def mute_off(chat_id: int):
+    mute[chat_id] = False
+
+
+async def get_active_chats() -> list:
+    return active
+
+
+async def is_active_chat(chat_id: int) -> bool:
+    if chat_id not in active:
+        return False
+    else:
+        return True
+
+
+async def add_active_chat(chat_id: int):
+    if chat_id not in active:
+        active.append(chat_id)
+
+
+async def remove_active_chat(chat_id: int):
+    if chat_id in active:
+        active.remove(chat_id)
