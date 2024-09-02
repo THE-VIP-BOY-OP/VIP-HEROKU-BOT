2a2a60e92cd57e01a42d7edccbed8e638b311464
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 08:47:05 +0530

    Update pytgcalls.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- Vivek/core/pytgcalls.py
+++ Vivek/core/pytgcalls.py
@@ -14,5 +14,20 @@ class MusicPlayer(PyTgCalls):
     async def leave_call(self, chat_id: int):
         await self.leave_call(chat_id)
 
+    async def mute_stream(self, chat_id: int):
+        await self.mute_stream(chat_id)
+
+    async def pause_stream(self, chat_id: int):
+        await self.pause_stream(chat_id)
+
+    async def resume_stream(self, chat_id: int):
+        await self.resume_stream(chat_id)
+
+    async def unmute_stream(self, chat_id: int):
+        await self.unmute_stream(chat_id)
+        
+        
+        
+
 
 call = MusicPlayer()
