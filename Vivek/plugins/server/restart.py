d391d23183327ff530c4f11ddb58f602dc308185
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 17:30:32 +0530

    Update restart.py

--- Vivek/plugins/server/restart.py
+++ Vivek/plugins/server/restart.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from Vivek import app
-
+from Vivek.utils import restart
 
 @app.on_message(filters.command(["restart"]) & filters.sudo & ~filters.forwarded)
 async def restart(_: Client, message: Message):
     await message.reply_text("<code>Restarting...</code>")
-    await app.restart()
+    await restart()
