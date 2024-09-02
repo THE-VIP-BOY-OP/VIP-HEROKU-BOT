421cea37b4c38135d1667a6922400eb17b0dac5b
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 08:33:49 +0530

    Update clients.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- Vivek/core/clients.py
+++ Vivek/core/clients.py
@@ -1,10 +1,12 @@
 from pyrogram import Client
 from pyrogram import __version__ as v
 
+from pytgcalls import PyTgCalls
+
 from config import API_HASH, API_ID, BOT_TOKEN, STRING_SESSION
 from Vivek.utils import VClient
 
-app = Client(
+"""app = Client(
     name="Vivek",
     api_id=API_ID,
     api_hash=API_HASH,
@@ -23,4 +25,34 @@ bot = VClient(
     in_memory=True,
     plugins=dict(root="Vivek/plugins/bot"),
     max_concurrent_transmissions=9,
-)
+)"""
+
+class App(Client):
+    def __init__(self):
+        super().__init__(
+            name="Vivek",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            app_version=f"Cute {v}",
+            session_string=STRING_SESSION,
+            in_memory=True,
+            plugins=dict(root="Vivek/plugins"),
+        )
+        self.call = PyTgCalls(self)
+        self.bot = VClient(
+            name="Vivek1",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            app_version=f"Cute {v}",
+            bot_token=BOT_TOKEN,
+            in_memory=True,
+            plugins=dict(root="Vivek/plugins/bot"),
+            max_concurrent_transmissions=9,
+        )
+
+    async def start(self):
+        await super().start()
+        await self.call.start()
+
+app = App()
+        
