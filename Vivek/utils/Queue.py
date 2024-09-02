d1d3919e4e834557ba9d033c87d1fd02741ba7b3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 20:52:04 +0530

    Update Queue.py

--- Vivek/utils/Queue.py
+++ Vivek/utils/Queue.py
@@ -1,15 +1,16 @@
-import asyncio
+import os
+from  asyncio import Queue, QueueEmpty
 from typing import Any, Dict, Optional
 
 
 class QueueManager:
     def __init__(self):
-        self.queues: Dict[int, asyncio.Queue] = {}
+        self.queues: Dict[int, Queue] = {}
 
     async def add_queue(self, chat_id: int, **params: Any):
         """Asynchronously add a set of parameters to the queue for a given chat_id."""
         if chat_id not in self.queues:
-            self.queues[chat_id] = asyncio.Queue()
+            self.queues[chat_id] = Queue()
         await self.queues[chat_id].put(params)
 
     async def retrieve(self, chat_id: int) -> Optional[Dict[str, Any]]:
@@ -17,25 +18,38 @@ class QueueManager:
         if chat_id in self.queues:
             try:
                 return self.queues[chat_id].get_nowait()
-            except asyncio.QueueEmpty:
+            except QueueEmpty:
                 return None
         return None
 
     async def remove(self, chat_id: int):
-        """Asynchronously remove the first set of parameters for the given chat_id."""
+        """Asynchronously remove the first set of parameters for the given chat_id, and delete file if file_path exists."""
         if chat_id in self.queues:
             try:
-                await self.queues[chat_id].get_nowait()
-            except asyncio.QueueEmpty:
+                params = self.queues[chat_id].get_nowait()
+                file_path = params.get('file_path')
+                if file_path and os.path.exists(file_path):
+                    os.remove(file_path)
+                return params
+            except QueueEmpty:
                 raise ValueError(f"No parameters to remove for chat_id {chat_id}")
         else:
             raise ValueError(f"No queue found for chat_id {chat_id}")
 
     async def clear_queue(self, chat_id: int):
-        """Asynchronously clear the entire queue for a given chat_id."""
+        """Asynchronously clear the entire queue for a given chat_id, deleting files if file_path exists."""
         if chat_id in self.queues:
+            while not self.queues[chat_id].empty():
+                try:
+                    params = self.queues[chat_id].get_nowait()
+                    file_path = params.get('file_path')
+                    if file_path and os.path.exists(file_path):
+                        os.remove(file_path)
+                except QueueEmpty:
+                    break
             self.queues.pop(chat_id, None)
 
     async def has_queue(self, chat_id: int) -> bool:
         """Check asynchronously if there is a queue for a given chat_id."""
         return chat_id in self.queues and not self.queues[chat_id].empty()
+ 
