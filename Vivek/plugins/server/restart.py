2788765c36ded8a0826f11c9fc613ecb872a6283
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 17:32:05 +0530

    Update restart.py

--- Vivek/plugins/server/restart.py
+++ Vivek/plugins/server/restart.py
@@ -6,6 +6,6 @@ from Vivek.utils import restart
 
 
 @app.on_message(filters.command(["restart"]) & filters.sudo & ~filters.forwarded)
-async def restart(_: Client, message: Message):
+async def restart_(_: Client, message: Message):
     await message.reply_text("<code>Restarting...</code>")
     await restart()
