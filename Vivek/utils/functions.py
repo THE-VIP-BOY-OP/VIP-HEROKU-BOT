8666eae9c79c463eec610a2b09205adc12d04f08
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 09:43:27 +0530

    Update functions.py

--- Vivek/utils/functions.py
+++ Vivek/utils/functions.py
@@ -1,6 +1,23 @@
 from pyrogram import Client
 
 
+pause = {}
+
 class VClient(Client):
     def __init__(self, name, *args, **kwargs):
         super().__init__(name, *args, **kwargs)
+
+
+async def is_music_playing(chat_id: int) -> bool:
+    mode = pause.get(chat_id)
+    if not mode:
+        return False
+    return mode
+
+
+async def music_on(chat_id: int):
+    pause[chat_id] = True
+
+
+async def music_off(chat_id: int):
+    pause[chat_id] = False
