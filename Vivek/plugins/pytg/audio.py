bccbb3e554aa479fe92bc8388ec0b858dc19124e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 10:40:33 +0530

    Update audio.py

--- Vivek/plugins/pytg/audio.py
+++ Vivek/plugins/pytg/audio.py
@@ -4,6 +4,9 @@ from config import LOG_GROUP_ID
 from Vivek import app
 
 
-@app.on_message(filters.sudo & filters.chat(LOG_GROUP_ID) & filters.audio)
+@app.on_message(filters.sudo & filters.chat(LOG_GROUP_ID) & (filters.audio | filters.voice))
 async def audio_play(client, message):
-    await message.reply_text(message)
+    if message.audio:
+        await message.reply_text(message.audio.file_id)
+    if message.voice:
+        await message.reply_text(message.voice.file_id)
