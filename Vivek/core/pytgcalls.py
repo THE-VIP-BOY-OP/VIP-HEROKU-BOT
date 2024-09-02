18246e6a2e45610aa39aec004ab18e7d49ced9b3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-02 07:58:17 +0530

    Update pytgcalls.py

--- Vivek/core/pytgcalls.py
+++ Vivek/core/pytgcalls.py
@@ -1,12 +1,12 @@
 from pytgcalls import PyTgCalls
 from pytgcalls.types import MediaStream
 
-from .clients import app
+from .clients import appp
 
 
 class MusicPlayer(PyTgCalls):
     def __init__(self):
-        super().__init__(app)
+        super().__init__(appp)
 
     async def play(self, file_path: str, chat_id: int):
         await self.play(chat_id, MediaStream(file_path))
