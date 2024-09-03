25b33b5942eae26be1803d61b91c0e76c05e1998
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-03 16:44:41 +0530

    Update update.py

--- Vivek/plugins/server/update.py
+++ Vivek/plugins/server/update.py
@@ -3,15 +3,14 @@ import os
 from datetime import datetime
 
 import aiohttp
-from git import Repo
+from git import Repo, remote
 from git.exc import GitCommandError, InvalidGitRepositoryError
 from pyrogram import filters
 
-from config import UPSTREAM_BRANCH
+from config import UPSTREAM_BRANCH, UPSTREAM_REPO, GIT_TOKEN
 from Vivek import app
 
 BASE = "https://batbin.me/"
-
 
 async def post(url: str, *args, **kwargs):
     async with aiohttp.ClientSession() as session:
@@ -22,7 +21,6 @@ async def post(url: str, *args, **kwargs):
                 data = await resp.text()
         return data
 
-
 async def paste(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
@@ -30,16 +28,29 @@ async def paste(text):
     link = BASE + resp["message"]
     return link
 
-
 @app.on_message(filters.command(["update", "up", "gitpull"]) & filters.sudo)
 async def update_(client, message):
     response = await message.reply_text("Checking for available updates.....")
     try:
         repo = Repo()
     except GitCommandError:
-        return await response.edit("Git commamd Error")
+        return await response.edit("Git command error")
     except InvalidGitRepositoryError:
-        return await response.edit("Invalid Git Respiratory")
+        if UPSTREAM_REPO:
+            repo_dir = os.getcwd()
+            repo = Repo.init(repo_dir)
+            origin = repo.create_remote('origin', UPSTREAM_REPO)
+            origin.fetch()
+            repo.create_head(UPSTREAM_BRANCH, origin.refs[UPSTREAM_BRANCH])
+            repo.heads[UPSTREAM_BRANCH].set_tracking_branch(origin.refs[UPSTREAM_BRANCH])
+            repo.heads[UPSTREAM_BRANCH].checkout()
+        else:
+            return await response.edit("Invalid Git repository and no UPSTREAM_REPO specified.")
+
+    origin = repo.remotes.origin
+
+    if GIT_TOKEN:
+        origin.set_url(f"https://{GIT_TOKEN}@{UPSTREAM_REPO.split('https://')[1]}")
 
     os.system(f"git fetch origin {UPSTREAM_BRANCH} &> /dev/null")
     await asyncio.sleep(7)
@@ -50,7 +61,7 @@ async def update_(client, message):
         verification = str(checks.count())
 
     if verification == "":
-        return await response.edit("Bot is Up To Date With Upstream Repo")
+        return await response.edit("Bot is up to date with upstream repo")
 
     updates = ""
     ordinal = lambda format: "%d%s" % (
@@ -60,13 +71,13 @@ async def update_(client, message):
     for info in repo.iter_commits(f"HEAD..origin/{UPSTREAM_BRANCH}"):
         updates += f"<b>➣ #{info.count()}: <a href={REPO_}/commit/{info}>{info.summary}</a> BY -> {info.author}</b>\n\t\t\t\t<b>➥ Commited On :</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
 
-    _update_response_ = "<b>A New Upadte Is Available For The Bot !</b>\n\n➣ Pushing Upadtes Now \n\n<b><u>Upadtes:</u></b>\n\n"
+    _update_response_ = "<b>A new update is available for the bot!</b>\n\n➣ Pushing updates now\n\n<b><u>Updates:</u></b>\n\n"
     _final_updates_ = _update_response_ + updates
 
     if len(_final_updates_) > 4096:
         url = await paste(updates)
         await response.edit(
-            f"<b>A New Upadte Is. Available For The Bot !</b>\n\n➣ Pushing Upadtes Now\n\n<u><b>Upadtes :</b></u>\n\n<a href={url}>Check Updates</a>"
+            f"<b>A new update is available for the bot!</b>\n\n➣ Pushing updates now\n\n<u><b>Updates:</b></u>\n\n<a href={url}>Check Updates</a>"
         )
     else:
         await response.edit(_final_updates_, disable_web_page_preview=True)
