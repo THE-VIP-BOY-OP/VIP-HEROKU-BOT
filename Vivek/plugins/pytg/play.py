a344d7d639ed2653dc36a84c2f8d4ee17cc751a7
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-04 14:29:43 +0530

    Update play.py

--- Vivek/plugins/pytg/play.py
+++ Vivek/plugins/pytg/play.py
@@ -103,7 +103,7 @@ async def play_command(client, message: Message):
     await Vivek.add_active_chat(message.chat.id)
     await app.send_message(
         message.chat.id,
-        f"**Started Streaming**\n\nTitle: {title}\nDuration: {duration_min}\nby {user_name}",
+        f"**Started Streaming**\n\n**Title**: {title}\n**Duration**: {duration_min}\n**by** {user_name}",
         disable_web_page_preview=True,
     )
 
