0ec034cedd74d8eec83fbb05d0e96f940d77fda3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-04 05:50:52 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -2,6 +2,11 @@ import uvloop
 
 uvloop.install()
 
+import logging
+import os
+import sys
+from os import listdir, mkdir
+
 from Vivek.core.clients import app, bot
 from Vivek.utils.filters import edit_filters
 
@@ -9,3 +14,17 @@ from .logger import LOGGER
 
 HELPABLE = {}
 edit_filters()
+
+for file in os.listdir():
+    if (
+        file.endswith(".jpg")
+        or file.endswith(".jpeg")
+        or file.endswith(".mp3")
+        or file.endswith(".png")
+        or file.endswith(".session")
+        or file.endswith(".session-journal")
+    ):
+        os.remove(file)
+
+if "downloads" not in listdir():
+        mkdir("downloads")
