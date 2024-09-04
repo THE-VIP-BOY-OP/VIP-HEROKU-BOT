55584d9d27ae220e6d778739c7a3912e136274a8
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-04 18:41:06 +0530

    Update pytg

---
+++ Vivek/plugins/pytg/endls.py
@@ -0,0 +1,22 @@
+from pyrogram import filters
+from Vivek import app
+
+chatlist = []
+
+@app.on_message(filters.sudo & filters.command("playendless"))
+async def endless(c, m):
+    chat_id = m.chat.id
+    if "on" in m.text.lower():
+        if chat_id not in chatlist:
+            chatlist.append(chat_id)
+            await m.reply("Endless playback mode is now **ON**.")
+        else:
+            await m.reply("Endless playback mode is already **ON**.")
+    elif "off" in m.text.lower():
+        if chat_id in chatlist:
+            chatlist.remove(chat_id)
+            await m.reply("Endless playback mode is now **OFF**.")
+        else:
+            await m.reply("Endless playback mode was not **ON**.")
+    else:
+        await m.reply("Usage: `!playendless on` to enable or `!playendless off` to disable.")
