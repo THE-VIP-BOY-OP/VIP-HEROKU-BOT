49eea59fff556c3cfd9a116a3924b1436e7dd048
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-04 10:01:27 +0530

    Update pytgcalls.py

--- Vivek/core/pytgcalls.py
+++ Vivek/core/pytgcalls.py
@@ -10,6 +10,9 @@ from Vivek.utils.functions import MelodyError
 
 from .clients import app
 
+from Vivek.utils.functions import Vivek
+from Vivek.utils.queue import Queue
+import os
 
 class MusicPlayer(PyTgCalls):
     def __init__(self):
@@ -65,7 +68,37 @@ class MusicPlayer(PyTgCalls):
         await super().resume_stream(chat_id)
 
     async def unmute_stream(self, chat_id: int):
-        await super().unmute_stream(chat_id)
+        await 
+super().unmute_stream(chat_id)
+
+    async def change_stream(chat_id):
+    	mystic = await app.send_message(chat_id, "Downloading Next track from Queue")
+        details = await Queue.next(chat_id)
+        if not details:
+        	await Vivek.remove_active_chat(chat_id)
+            await mystic.edit(chat_id, "No More songs in Queue Leaving Voice Chat")
+            return await self.leave_call(chat_id)
+        
+        title = details.get('title')
+        duration = details.get('duration')
+        vidid = details.get('vidid')
+        video = details.get('video')
+        file_path = details.get('file_path')
+        by = details.get('by')
+        if not os.path.isfile(file_path):
+        	file_path = await Vivek.download(vidid=vidid, video=video)
+        try:
+            await call.play(message.chat.id, file_path, video)
+        except MelodyError as e:
+                return await mystic.edit(e)
+        except Exception as e:
+            return await mystic.edit(e)
+        await app.send_message(
+            message.chat.id,
+            f"**Started Streaming**\n\nTitle: {title}\nDuration: {duration}\n by {by}",
+            disable_web_page_preview=True,
+        )
+    
 
     async def dec(self):
         @super().on_update(filters.stream_end)
