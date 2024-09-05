35d50517e144e6a2fb199d2fca575696cc22963a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-05 15:37:03 +0530

    Update update.py

--- Vivek/plugins/server/update.py
+++ Vivek/plugins/server/update.py
@@ -5,26 +5,7 @@ from pyrogram import filters
 
 from Vivek import app
 
-BASE = "https://batbin.me/"
-
-
-async def post(url: str, *args, **kwargs):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, *args, **kwargs) as resp:
-            try:
-                data = await resp.json()
-            except Exception:
-                data = await resp.text()
-        return data
-
-
-async def paste(text):
-    resp = await post(f"{BASE}api/v2/paste", data=text)
-    if not resp["success"]:
-        return
-    link = BASE + resp["message"]
-    return link
-
+ 
 
 @app.on_message(filters.command(["update", "up", "gitpull"]) & filters.sudo)
 async def update_(client, message):
