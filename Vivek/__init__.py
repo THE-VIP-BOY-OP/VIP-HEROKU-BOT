88012f1be9bd691f0316fd69a237d3e3ecf8b7e1
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-07 05:07:41 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -16,6 +16,43 @@ from .logger import LOGGER
 HELPABLE = {}
 edit_filters()
 
+
+import os
+import sys
+
+from pyrogram import Client
+from pyrogram import __version__ as v
+
+from config import API_HASH, API_ID, BOT_TOKEN, STRING_SESSION
+
+app = Client(
+    name="Vivek",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    app_version=f"Cute {v}",
+    session_string=STRING_SESSION,
+    in_memory=True,
+    plugins=dict(root="Vivek/plugins"),
+)
+bot = Client(
+    name="Vivek1",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    app_version=f"Cute {v}",
+    bot_token=BOT_TOKEN,
+    in_memory=True,
+    plugins=dict(root="Vivek/plugins"),
+    max_concurrent_transmissions=9,
+)
+
+
+async def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "Vivek", *sys.argv[1:]])
+
+
+app.bot = bot
+app.restart = restart
+
 for file in os.listdir():
     if (
         file.endswith(".jpg")
