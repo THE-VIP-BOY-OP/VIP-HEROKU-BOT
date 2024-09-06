f39c9b4270cc452a4d806038385ccdefe14a5bc6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-06 20:13:53 +0530

    Update functions.py

--- Vivek/utils/functions.py
+++ Vivek/utils/functions.py
@@ -11,6 +11,14 @@ from youtubesearchpython.__future__ import VideosSearch
 
 from config import LOG_GROUP_ID
 from Vivek.logger import LOGGER
+from datetime import datetime, timedelta
+from re import findall
+from re import sub as re_sub
+
+from pyrogram import errors
+from pyrogram.enums import MessageEntityType
+from pyrogram.types import Message
+
 
 log = LOGGER(__name__)
 
@@ -20,10 +28,6 @@ mute = {}
 active = []
 chatlist = []
 
-
-class VClient(Client):
-    def __init__(self, name, *args, **kwargs):
-        super().__init__(name, *args, **kwargs)
 
 
 class MelodyError(Exception):
@@ -210,3 +214,80 @@ class Vivek:
     async def remove_active_chat(chat_id: int):
         if chat_id in active:
             active.remove(chat_id)
+    
+    async def extract_userid(message, text: str):
+        """
+        NOT TO BE USED OUTSIDE THIS FILE
+        """
+
+        def is_int(text: str):
+            try:
+                int(text)
+            except ValueError:
+                return False
+            return True
+
+        text = text.strip()
+
+        if is_int(text):
+            return int(text)
+
+        entities = message.entities
+        app = message._client
+        if len(entities) < 2:
+            return (await app.get_users(text)).id
+        entity = entities[1]
+        if entity.type == MessageEntityType.MENTION:
+            return (await app.get_users(text)).id
+        if entity.type == MessageEntityType.TEXT_MENTION:
+            return entity.user.id
+        return None
+
+
+    async def extract_user_and_reason(message, sender_chat=False):
+        args = message.text.strip().split()
+        text = message.text
+        user = None
+        reason = None
+
+        try:
+            if message.reply_to_message:
+                reply = message.reply_to_message
+                # if reply to a message and no reason is given
+                if not reply.from_user:
+                    if (
+                    reply.sender_chat
+                    and reply.sender_chat != message.chat.id
+                    and sender_chat
+                    ):
+                        id_ = reply.sender_chat.id
+                    else:
+                        return None, None
+                else:
+                    id_ = reply.from_user.id
+
+                if len(args) < 2:
+                    reason = None
+                else:
+                    reason = text.split(None, 1)[1]
+                return id_, reason
+
+            # if not reply to a message and no reason is given
+            if len(args) == 2:
+                user = text.split(None, 1)[1]
+                return await extract_userid(message, user), None
+
+            # if reason is given
+            if len(args) > 2:
+                user, reason = text.split(None, 2)[1:]
+                return await extract_userid(message, user), reason
+
+            return user, reason
+
+        except errors.UsernameInvalid:
+            return "", ""
+
+
+    async def extract_user(message):
+        return (await extract_user_and_reason(message))[0]
+
