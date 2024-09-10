4e3d820d34d34ed2f48d6f82385f40709c99d180
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-10 09:29:56 +0530

    Update audio.py

--- Vivek/plugins/pytg/audio.py
+++ Vivek/plugins/pytg/audio.py
@@ -5,34 +5,73 @@ from pyrogram import filters
 
 from config import LOG_GROUP_ID
 from Vivek import MusicPlayer, app
-from Vivek.utils.functions import S12K, Vivek
+from Vivek.utils.functions import S12K, Vivek, MelodyError
 from Vivek.utils.queue import Queue
-
 
 @app.on_message(filters.chat(LOG_GROUP_ID) & (filters.audio | filters.voice))
 async def audio_play(client, message):
+	mystic = await message.reply_text("processing")
+	
     if message.audio:
-        file_path = message.audio.file_id
+        file_id = message.audio.file_id
         file_name = message.audio.file_name
+        duration = message.audio.duration
     if message.voice:
-        file_path = message.voice.file_id
+        file_id = message.voice.file_id
+        duration = message.voice.duration
         file_name = f"{message.voice.file_unique_id}.ogg"
-
-    a = await app.download_media(file_path)
+        
+        
+    file_path = os.path.join("downloads", file_name)
+    user_name = "🤗"
+    a = await app.download_media(file_id)
 
     (
         ffmpeg.input(a)
         .filter("volume", "18dB")
-        .output(file_name)
+        .output(file_path)
         .overwrite_output()
         .run()
     )
     chat_id = S12K()
-    await MusicPlayer.play(chat_id, file_name)
-    await message.reply_text("Started Playing")
-    os.remove(a)
-    os.remove(file_name)
+    title = "Playing Local Audio/Voice"
+    
+    if await Vivek.is_active_chat(chat_id):
+        await Queue.add(
+            chat_id,
+            title=title,
+            duration=duration_min,
+            vidid=None,
+            video=False,
+            file_path=file_path,
+            by=user_name,
+        )
+        count = len(await Queue.get_queues(chat_id)) - 1
+        return await mystic.edit(
+            f"<b>Added To Queue At {count}</b>\n<b>Title:</b> {title}\n<b>Duration</b>: {duration_min}\n<b>By</b>: {user_name}",
+            disable_web_page_preview=True,
+            parse_mode=ParseMode.HTML,
+        )
 
+    try:
+        await MusicPlayer.play(message.chat.id, file_path, video)
+    except MelodyError as e:
+        return await mystic.edit(e)
+    except Exception as e:
+        return await mystic.edit(e)
+    await Queue.add(
+        chat_id,
+        title=title,
+        duration=duration_min,
+        vidid=None,
+        video=False,
+        file_path=file_path,
+        by=user_name,
+    )
+
+    await Vivek.add_active_chat(message.chat.id)
+    await mystic.edit("Started Playing")
+    os.remove(a)
 
 @app.on_message(filters.sudo & filters.command("playhere"))
 async def audio_play(client, message):
@@ -40,11 +79,3 @@ async def audio_play(client, message):
     await message.reply_text(
         "Now All recieved audio/voice has been playing here from since Now"
     )
-
-
-@app.on_message(filters.sudo & filters.command("end"))
-async def audio_play(client, message):
-    chat_id = message.chat.id
-    await MusicPlayer.leave_call(chat_id)
-    await Queue.clear(chat_id)
-    await Vivek.remove_active_chat(chat_id)
