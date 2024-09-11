8ec8a7147a38f287f2f5ff6ea3881051974bb944
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-11 18:30:23 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -5,44 +5,13 @@ import os
 import shutil
 import sys
 
-import pyromod.listen  # noqa
-from pyrogram import Client
-from pyrogram import __version__ as v
 
-from config import API_HASH, API_ID, BOT_TOKEN, STRING_SESSION
-from Vivek.core.logger import LOGGER
-from Vivek.utils.filters import edit_filters
+
+from .core.logger import LOGGER
+from .functions.filters import edit_filters
 
 edit_filters()
 
-app = Client(
-    name="Vivek",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    app_version=f"Cute {v}",
-    session_string=STRING_SESSION,
-    in_memory=True,
-    plugins=dict(root="Vivek/plugins"),
-)
-bot = Client(
-    name="Vivek1",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    app_version=f"Cute {v}",
-    bot_token=BOT_TOKEN,
-    in_memory=True,
-    plugins=dict(root="Vivek/plugins"),
-    max_concurrent_transmissions=9,
-)
-
-
-async def restart():
-    os.execvp(sys.executable, [sys.executable, "-m", "Vivek", *sys.argv[1:]])
-
-
-app.bot = bot
-app.restart = restart
-app.call = call
 
 
 for file in os.listdir():
