e8801d80ff97032f420e8033a59f949c8e5c17ad
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-11 21:19:20 +0530

    Update client.py

--- Vivek/core/client.py
+++ Vivek/core/client.py
@@ -4,25 +4,32 @@ from pyrogram import __version__ as v
 from config import API_HASH, API_ID, BOT_TOKEN, STRING_SESSION
 from Vivek.functions.client import VClient
 
-app = VClient(
-    name="Vivek",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    app_version=f"Cute {v}",
-    session_string=STRING_SESSION,
-    in_memory=True,
-    plugins=dict(root="Vivek/plugins"),
-)
-bot = VClient(
-    name="Vivek1",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    app_version=f"Cute {v}",
-    bot_token=BOT_TOKEN,
-    in_memory=True,
-    plugins=dict(root="Vivek/plugins"),
-    max_concurrent_transmissions=9,
-)
+
+class App(VClient):
+    def __init__(self):
+        super().__init__(
+            name="Vivek",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            app_version=f"Cute {v}",
+            session_string=STRING_SESSION,
+            in_memory=True,
+        )
+
+        self.bot = VClient(
+                name="Vivek1",
+                api_id=API_ID,
+                api_hash=API_HASH,
+                app_version=f"Cute {v}",
+                bot_token=BOT_TOKEN,
+                in_memory=True,
+                max_concurrent_transmissions=9,
+            )
+
+    
+    async def start(self):
+        await super().start()
+        await self.bot.start()
 
 
-app.bot = bot
+app = App()
