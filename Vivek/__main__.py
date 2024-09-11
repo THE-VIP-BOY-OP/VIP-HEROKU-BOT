84aef5f478f1e1b8577fcee3ae242f92d276501a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-11 21:27:47 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -9,18 +9,8 @@ from Vivek.plugins import ALL_MODULES
 
 
 async def main():
-    LOGGER("Vivek").info("Staring Clients")
-
     await app.start()
-    await app.bot.start()
-    await app.bot.send_message(LOG_GROUP_ID, "started")
-    for all_module in ALL_MODULES:
-        importlib.import_module("Vivek.plugins" + all_module)
-    LOGGER("Vivek").info("Clients Started Successfully")
-    await idle()
 
-    await app.stop()
-    await app.bot.stop()
 
 
 if __name__ == "__main__":
