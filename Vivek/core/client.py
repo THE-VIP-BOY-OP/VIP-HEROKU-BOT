971aa4c8727c4cb29df8cc63ec8ed20b1c5ab4e7
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-11 18:31:33 +0530

    Update core

---
+++ Vivek/core/client.py
@@ -0,0 +1,32 @@
+import pyromod.listen  # noqa
+from pyrogram import Client
+from pyrogram import __version__ as v
+from config import API_HASH, API_ID, BOT_TOKEN, STRING_SESSION
+
+app = Client(
+    name="Vivek",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    app_version=f"Cute {v}",
+    session_string=STRING_SESSION,
+    in_memory=True,
+    plugins=dict(root="Vivek/plugins"),
+)
+bot = Client(
+    name="Vivek1",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    app_version=f"Cute {v}",
+    bot_token=BOT_TOKEN,
+    in_memory=True,
+    plugins=dict(root="Vivek/plugins"),
+    max_concurrent_transmissions=9,
+)
+
+
+async def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "Vivek", *sys.argv[1:]])
+
+
+app.bot = bot
+app.restart_script = restart
