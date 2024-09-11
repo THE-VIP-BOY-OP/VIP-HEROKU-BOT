a5b84a34f86c1d83f57166081718240148f15a53
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-11 18:37:14 +0530

    Update client.py

--- Vivek/functions/client.py
+++ Vivek/functions/client.py
@@ -8,5 +8,5 @@ class VClient(Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    async def restart():
+    async def restart_script():
         os.execvp(sys.executable, [sys.executable, "-m", "Vivek", *sys.argv[1:]])
