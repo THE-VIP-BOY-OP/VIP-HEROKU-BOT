861c10bf7a5aa43bae2d27df5bf5c3e2ee8d4f0c
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-11 20:44:09 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -4,7 +4,7 @@ import importlib
 from pyrogram import idle
 
 from config import LOG_GROUP_ID
-from Vivek import HELPABLE, LOGGER, app, call
+from Vivek import LOGGER, app
 from Vivek.plugins import ALL_MODULES
 
 
@@ -15,11 +15,7 @@ async def main():
     await app.bot.start()
     await app.bot.send_message(LOG_GROUP_ID, "started")
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module("Vivek.plugins" + all_module)
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    await call.start()
+        importlib.import_module("Vivek.plugins" + all_module)
     LOGGER("Vivek").info("Clients Started Successfully")
     await idle()
 
