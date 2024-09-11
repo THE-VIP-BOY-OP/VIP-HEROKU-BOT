7ce5c260ab0cd7d9256cdeac04e38d177c4cb8e8
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-11 18:36:27 +0530

    Update client.py

--- Vivek/functions/client.py
+++ Vivek/functions/client.py
@@ -1,6 +1,11 @@
 from pyrogram import Client
-
+import os, sys
 
 class VClient(Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+    async def restart():
+        os.execvp(sys.executable, [sys.executable, "-m", "Vivek", *sys.argv[1:]])
+
