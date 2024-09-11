1c0ce7c31022093e6b8e6aa6619ae79aed55d8a5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-11 21:57:46 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -1,11 +1,6 @@
-import asyncio
-
 from Vivek import app
 
 
-async def main():
-    await app.start()
-
 
 if __name__ == "__main__":
-    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(main())
+    app.run(app.start)
