01509d930a97d837871b900e553aae849ba230ed
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-12 13:13:13 +0530

    Update client.py

--- Vivek/functions/client.py
+++ Vivek/functions/client.py
@@ -2,11 +2,12 @@ import os
 import sys
 
 from pyrogram import Client
-
+from .help import BotHelp
 
 class VClient(Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.help = BotHelp
 
     async def restart_script():
         os.execvp(sys.executable, [sys.executable, "-m", "Vivek", *sys.argv[1:]])
