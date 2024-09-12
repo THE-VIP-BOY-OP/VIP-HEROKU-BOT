2d1c6cde82312af36b4288568dfa2ee359d2d6a6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-12 07:37:00 +0530

    Update dir.py

--- Vivek/functions/dir.py
+++ Vivek/functions/dir.py
@@ -1,8 +1,6 @@
 import os
 import shutil
 
-from ..core import LOGGER
-
 
 def dir():
     files = [
@@ -24,4 +22,3 @@ def dir():
     if "downloads" in os.listdir():
         shutil.rmtree("downloads")
         os.mkdir("downloads")
-    LOGGER(__name__).info("Files refreshed")
