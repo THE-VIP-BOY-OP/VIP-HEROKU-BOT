4e78905b1f69570321356bfd7087593078116cb1
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-13 12:25:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,33 @@
-        "API_ID",
-    )
-    getenv(
-    map(int, getenv("OWNER_ID", "6815918609 7301077117 7428672286").split())
-)
-)
-API_HASH = getenv("API_HASH", "")
-API_ID = int(
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-GIT_TOKEN = getenv("GIT_TOKEN", "")
-LOG_GROUP_ID = getenv("LOG_GROUP_ID", -1002146211959)
-ONLY_FOR_SUDO = bool(getenv("ONLY_FOR_SUDO", True))
-OWNER_ID = list(
-PREFIX = list(map(str, getenv("PREFIX", "!").split()))
-STRING_SESSION = getenv("STRING_SESSION", None)
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Vivekkumar-IN/Vivek")
-from dotenv import load_dotenv
 from os import getenv
+
+from dotenv import load_dotenv
+
 load_dotenv()
 
+API_ID = int(
+    getenv(
+        "API_ID",
+    )
+)
+API_HASH = getenv("API_HASH", "")
+
+STRING_SESSION = getenv("STRING_SESSION", None)
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+
+LOG_GROUP_ID = getenv("LOG_GROUP_ID", -1002146211959)
+
+OWNER_ID = list(
+    map(int, getenv("OWNER_ID", "6815918609 7301077117 7428672286").split())
+)
+
+PREFIX = list(map(str, getenv("PREFIX", "!").split()))
 
 
+ONLY_FOR_SUDO = bool(getenv("ONLY_FOR_SUDO", True))
 
+GIT_TOKEN = getenv("GIT_TOKEN", "")
 
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
-
-
-
-
-
-
-
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Vivekkumar-IN/Vivek")
