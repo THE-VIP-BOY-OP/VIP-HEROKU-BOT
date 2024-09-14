b6633d8d4a231f5bee96e4dabf96f723bbad68bb
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-14 10:59:37 +0530

    Update search.py

--- Vivek/plugins/tools/search.py
+++ Vivek/plugins/tools/search.py
@@ -3,13 +3,14 @@ from youtubesearchpython.__future__ import VideosSearch
 
 from Vivek import app
 
-
 @app.on_message(filters.command(["search"]))
 async def ytsearch(client, message):
-    m = await message.reply_text("Searching....")
+    m = await message.reply_text("🔍 Searching on YouTube...")
     try:
         if len(message.command) < 2:
-            return await m.edit_text("Provide A Song/Video Name To Search In Youtube")
+            return await m.edit_text(
+                "❗ Provide a song/video name to search on YouTube."
+            )
         query = message.text.split(None, 1)[1]
         results = VideosSearch(query, limit=1)
         for result in (await results.next())["result"]:
@@ -17,10 +18,24 @@ async def ytsearch(client, message):
             duration_min = result["duration"]
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
             vidid = result["id"]
-        text = f"**Title** : {title}\n**Duration**: {duration_min}\n**Thumbnail**: {thumbnail}\n**Link**: https://www.youtube.com/watch?v={vidid}"
+            channel = result["channel"]["name"]
+            views = result["viewCount"]["short"]
+            upload_date = result["publishedTime"]
+
+        text = (
+            f"**🎵 Title**: [{title}](https://www.youtube.com/watch?v={vidid})\n"
+            f"**⏱ Duration**: {duration_min}\n"
+            f"**📅 Uploaded**: {upload_date}\n"
+            f"**👁 Views**: {views}\n"
+            f"**📺 Channel**: {channel}\n"
+            f"**🔗 [Watch on YouTube](https://www.youtube.com/watch?v={vidid})**"
+        )
+        
         await m.edit_text(
             text=text,
             disable_web_page_preview=True,
         )
     except Exception as e:
-        await m.edit_text(str(e))
+        await m.edit_text(f"⚠️ An error occurred: {str(e)}")
+
+app.help("Youtube").info("Searches YouTube for a video and provides details like title, views.").add("search", "Provide a <query> to search for videos or songs on YouTube.").done()
