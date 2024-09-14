0edfb87b65f1b226e73485c34e9b094c3275978b
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-14 14:13:39 +0530

    Update help.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- Vivek/plugins/bot/help.py
+++ Vivek/plugins/bot/help.py
@@ -173,3 +173,15 @@ async def inline_query_handler(client, query):
     if text == "RadheRadhe_GiveMeHelp":
         photo = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
         buttons = paginate_modules(0, BOT_CMD_MENU, "help")
+        answers.append(
+                InlineQueryResultPhoto(
+                    photo_url=photo,
+                    thumb_url=photo,
+                    caption="Here are the help of my Plugins",
+                    reply_markup=buttons,
+                )
+        )
+        try:
+            return await client.answer_inline_query(query.id, results=answers)
+        except:
+            return
