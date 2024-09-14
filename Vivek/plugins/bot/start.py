0f3b14cd61bf8e23dd4ee2b5729b0e0e7dcee6ff
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-14 21:34:55 +0530

    Update bot

---
+++ Vivek/plugins/bot/start.py
@@ -0,0 +1,11 @@
+from pyrogram import filters 
+from Vivek import app
+
+
+
+@app.bot.on_message(filters.command(["start"]) & filters.private)
+@LanguageStart
+async def start_comm(client, message):
+    if len(message.text.split()) > 1:
+        dec_fileid = message.text.split(None, 1)[1]
+        await message.reply_text(dec_fileid)
