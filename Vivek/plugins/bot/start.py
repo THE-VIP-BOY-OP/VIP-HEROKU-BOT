37ef5d7684c4ec4beb03ee77c31bbb5e41be2266
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-15 06:30:23 +0530

    Update start.py

--- Vivek/plugins/bot/start.py
+++ Vivek/plugins/bot/start.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from Vivek import app
 
 
-@app.bot.on_message(filters.command(["start"]) & filters.private)
+@app.bot.on_message(filters.commmand(["start"]) & filters.private)
 async def start_comm(client, message):
     if len(message.text.split()) > 1:
         dec_fileid = message.text.split(None, 1)[1]
