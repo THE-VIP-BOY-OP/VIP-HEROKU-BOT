6511fdd75e8c2edad9866a1e91389d86c858176c
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-15 06:28:50 +0530

    Update start.py

--- Vivek/plugins/bot/start.py
+++ Vivek/plugins/bot/start.py
@@ -4,7 +4,6 @@ from Vivek import app
 
 
 @app.bot.on_message(filters.command(["start"]) & filters.private)
-@LanguageStart
 async def start_comm(client, message):
     if len(message.text.split()) > 1:
         dec_fileid = message.text.split(None, 1)[1]
