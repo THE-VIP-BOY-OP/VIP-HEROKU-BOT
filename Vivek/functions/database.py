54af698540c4abdc0e960105d1b25b1ab021ad92
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-15 15:58:35 +0530

    Update database.py

--- Vivek/functions/database.py
+++ Vivek/functions/database.py
@@ -1,6 +1,5 @@
 import aiosqlite
 
-
 class DB:
     def __init__(self, db_name: str):
         self.db_name = db_name
@@ -8,32 +7,29 @@ class DB:
     async def _connect(self):
         return await aiosqlite.connect(self.db_name)
 
-    async def create_table(self, table_name: str):
+    async def _initialize_table(self, table_name: str):
         async with await self._connect() as db:
-            await db.execute(
-                f"""
+            await db.execute(f'''
                 CREATE TABLE IF NOT EXISTS {table_name} (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     my_info TEXT
                 )
-            """
-            )
+            ''')
             await db.commit()
 
-    async def save_my_data(self, table_name: str = "my_info", **kwargs):
+    async def save_my_data(self, table_name: str, **kwargs):
+        await self._initialize_table(table_name)
         async with await self._connect() as db:
             data = str(kwargs)
-            await db.execute(
-                f"""
+            await db.execute(f'''
                 INSERT INTO {table_name} (my_info) VALUES (?)
-            """,
-                (data,),
-            )
+            ''', (data,))
             await db.commit()
 
-    async def get_my_data(self, table_name: str = "my_info"):
+    async def get_my_data(self, table_name: str):
+        await self._initialize_table(table_name)
         async with await self._connect() as db:
-            async with db.execute(f"SELECT * FROM {table_name}") as cursor:
+            async with db.execute(f'SELECT * FROM {table_name}') as cursor:
                 rows = await cursor.fetchall()
                 result = [{"id": row[0], **eval(row[1])} for row in rows]
                 return result
