721bc913926732d63038f09375081fa48481ea59
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-17 21:19:57 +0530

    Update client.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- Vivek/functions/client.py
+++ Vivek/functions/client.py
@@ -3,7 +3,7 @@ import sys
 
 from pyrogram import Client
 from pyrogram.enums import MessageMediaType, MessagesFilter
-
+from pyrogram.types import InputMediaDocument
 from config import DATABASE_CHANNEL_ID
 
 from .help import BotHelp
@@ -31,7 +31,13 @@ class VClient(Client):
         if len(messages) == 0:
             return False
 
-        msg = messages[0]
+        msg = max(
+            messages, key=lambda msg: max(msg.date, msg.edit_date or msg.date)
+        )
+        for message in messages:
+            if message.id != msg.id:
+                await self.delete_messages(DATABASE_CHANNEL_ID, message.id)
+            
 
         if os.path.isfile(".mydatabase.db"):
             try:
@@ -45,3 +51,29 @@ class VClient(Client):
         await msg.download(file_name=file_path)
 
         return os.path.isfile(".mydatabase.db")
+
+    async def export_database(self):
+        messages = []
+
+        async for message in self.search_messages(
+            DATABASE_CHANNEL_ID, filter=MessagesFilter.PINNED
+        ):
+            if message.media == MessageMediaType.DOCUMENT:
+                if message.caption and "DATABASE" in message.caption:
+                    if message.document.file_name.endswith(".db"):
+                        messages.append(message)
+
+        if len(messages) == 0:
+            return False
+
+        msg = max(
+            messages, key=lambda msg: max(msg.date, msg.edit_date or msg.date)
+        )
+        for message in messages:
+            if message.id != msg.id:
+                await self.delete_messages(DATABASE_CHANNEL_ID, message.id)
+            
+
+        if os.path.isfile(".mydatabase.db"):
+            media = InputMediaDocument(media=".mydatabase.db", caption=f"this is DATABASE of {self.me.mention} Please don't Delete or Unpin This message\nElse your bot data will be deleted")
+            await self.edit_message_media(chat_id=DATABASE_CHANNEL_ID, message_id=msg.id, media=media, file_name=".mydatabase.db")
