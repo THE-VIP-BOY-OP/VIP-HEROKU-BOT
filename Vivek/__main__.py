3200fcb6bb51217321ad09d089b4e952a990ea61
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-18 15:51:52 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -1,41 +1,4 @@
-import asyncio
-import atexit
-import signal
-import sys
-
-from pyrogram import idle
-
 from Vivek import app
 
-loop = asyncio.get_event_loop_policy().get_event_loop()
-
-
-def run_shutdown():
-    app.stop()
-
-
-def handle_signal(signal_number, frame):
-    run_shutdown()
-    loop.stop()
-    sys.exit(0)
-
-
-def handle_exit():
-    run_shutdown()
-
-
-async def main():
-    atexit.register(handle_exit)
-    signal.signal(signal.SIGINT, handle_signal)
-    signal.signal(signal.SIGTERM, handle_signal)
-    signal.signal(signal.SIGQUIT, handle_signal)
-    await app.start()
-    await idle()
-
-
 if __name__ == "__main__":
-    try:
-        loop.run_until_complete(main())
-    except Exception as e:
-        run_shutdown()
-        raise e
+    app.run(app.start)
