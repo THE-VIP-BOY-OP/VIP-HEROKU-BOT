aa4e6bcd811c3735d54121a902f0b344b14b3be6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-18 15:48:51 +0530

    Update data.py

--- Vivek/plugins/tools/data.py
+++ Vivek/plugins/tools/data.py
@@ -1,14 +1,13 @@
+import asyncio
 from pyrogram.enums import MessageMediaType, MessagesFilter
 from pyrogram.types import InputMediaDocument
-
 from config import DATABASE_CHANNEL_ID
-
 from datetime import datetime
 import pytz
+import os
 
-async def export_database(self):
+async def export_database():
     messages = []
-
     async for message in app.search_messages(
         DATABASE_CHANNEL_ID, filter=MessagesFilter.PINNED
     ):
@@ -17,24 +16,36 @@ async def export_database(self):
                 if message.document.file_name.endswith(".db"):
                     messages.append(message)
 
-                    if len(messages) == 0:
-                        return False
+    if len(messages) == 0:
+        return False
 
     msg = max(messages, key=lambda msg: max(msg.date, msg.edit_date or msg.date))
     for message in messages:
         if message.id != msg.id:
-            await self.delete_messages(DATABASE_CHANNEL_ID, message.id)
+            await app.delete_messages(DATABASE_CHANNEL_ID, message.id)
 
     if os.path.isfile(".mydatabase.db"):
         time = datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%Y-%m-%d %H:%M:%S")
 
         media = InputMediaDocument(
-                media=".mydatabase.db",
-                caption=f"> this is DATABASE of {self.me.mention} Please don't Delete or Unpin This message\n> Else your bot data will be deleted\n Refreshed at {time}",
+            media=".mydatabase.db",
+            caption=(
+                f"> this is DATABASE. Please don't Delete or Unpin This message\n"
+                "> Else your bot data will be deleted\n"
+                f"Refreshed at {time}"
+            ),
         )
-       await app.edit_message_media(
-                chat_id=DATABASE_CHANNEL_ID,
-                message_id=msg.id,
-                media=media,
-                file_name=".mydatabase.db",
-            )
+        await app.edit_message_media(
+            chat_id=DATABASE_CHANNEL_ID,
+            message_id=msg.id,
+            media=media,
+            file_name=".mydatabase.db",
+        )
+
+async def run_export_stop():
+    while True:
+        await export_database()
+        await asyncio.sleep(3600)
+
+
+asyncio.create_task(run_export_stop())
