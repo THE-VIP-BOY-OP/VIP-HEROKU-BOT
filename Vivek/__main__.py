f99a674f834305adcaa57d60b1ae3788df082bf2
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-18 09:10:53 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -10,19 +10,18 @@ from Vivek import app
 loop = asyncio.get_event_loop_policy().get_event_loop()
 
 
-async def run_shutdown():
-    await app.stop()
+def run_shutdown():
+    app.stop()
 
 
 def handle_signal(signal_number, frame):
-    asyncio.ensure_future(run_shutdown())
+    run_shutdown()
     loop.stop()
     sys.exit(0)
 
 
 def handle_exit():
-    # Schedule shutdown asynchronously if event loop is running
-    asyncio.ensure_future(run_shutdown())
+    run_shutdown()
 
 
 async def main():
@@ -38,9 +37,5 @@ if __name__ == "__main__":
     try:
         loop.run_until_complete(main())
     except Exception as e:
-        # Use asyncio.ensure_future if loop is running
-        if loop.is_running():
-            asyncio.ensure_future(run_shutdown())
-        else:
-            loop.run_until_complete(run_shutdown())
+        run_shutdown()
         raise e
