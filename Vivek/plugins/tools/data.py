2a47e85ea7f4727aaa88333728f315e40afb1599
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-18 15:46:02 +0530

    Update data.py

--- Vivek/plugins/tools/data.py
+++ Vivek/plugins/tools/data.py
@@ -1,1 +1,40 @@
+from pyrogram.enums import MessageMediaType, MessagesFilter
+from pyrogram.types import InputMediaDocument
 
+from config import DATABASE_CHANNEL_ID
+
+from datetime import datetime
+import pytz
+
+async def export_database(self):
+    messages = []
+
+    async for message in app.search_messages(
+        DATABASE_CHANNEL_ID, filter=MessagesFilter.PINNED
+    ):
+        if message.media == MessageMediaType.DOCUMENT:
+            if message.caption and "DATABASE" in message.caption:
+                if message.document.file_name.endswith(".db"):
+                    messages.append(message)
+
+                    if len(messages) == 0:
+                        return False
+
+    msg = max(messages, key=lambda msg: max(msg.date, msg.edit_date or msg.date))
+    for message in messages:
+        if message.id != msg.id:
+            await self.delete_messages(DATABASE_CHANNEL_ID, message.id)
+
+    if os.path.isfile(".mydatabase.db"):
+        time = datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%Y-%m-%d %H:%M:%S")
+
+        media = InputMediaDocument(
+                media=".mydatabase.db",
+                caption=f"> this is DATABASE of {self.me.mention} Please don't Delete or Unpin This message\n> Else your bot data will be deleted\n Refreshed at {time}",
+        )
+       await app.edit_message_media(
+                chat_id=DATABASE_CHANNEL_ID,
+                message_id=msg.id,
+                media=media,
+                file_name=".mydatabase.db",
+            )
