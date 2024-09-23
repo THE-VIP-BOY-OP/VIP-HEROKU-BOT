169bfc571568802377302e4c06ea460a153f6535
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-23 15:40:00 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -1,27 +1,15 @@
 import uvloop
+uvloop.install() # Applying uvloop event loop policy
+from .functions.logger import LOGGER # Logger 
+from .functions import edit_filters, dir # functions for editing pyrogram filters or for removing media's on starting
+from Vivek.functions.client import VClient # This is pyrogram modified Client 
+from pyrogram import __version__ as v
+from config import API_HASH, API_ID, BOT_TOKEN, LOG_GROUP_ID, STRING_SESSION
 
-# Applying uvloop event loop policy
-
-uvloop.install()
-
-from Vivek.functions.client import VClient  # This is pyrogram modified Client
-
-from .functions import dir, edit_filters
-
-# Logger
-from .functions.logger import LOGGER
-
-# functions for editing pyrogram filters or for removing media's on starting
 
 
 dir()
 edit_filters()
-
-import pyromod.listen  # noqa
-from pyrogram import __version__ as v
-
-from config import API_HASH, API_ID, BOT_TOKEN, LOG_GROUP_ID, STRING_SESSION
-
 app = Vclient(
     name="Vivek",
     api_id=API_ID,
@@ -31,6 +19,7 @@ app = Vclient(
     in_memory=True,
 )
 
+# Initialising bot client as app.bot
 app.bot = VClient(
     name="Vivek1",
     api_id=API_ID,
