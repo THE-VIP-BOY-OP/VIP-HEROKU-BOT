17e877695f5f103bd2d33e12e94aa935351df764
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-23 21:10:16 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -1,15 +1,17 @@
 import uvloop
 
 uvloop.install()  # Applying uvloop event loop policy
+
+import os
+import shutil
+
 from pyrogram import __version__ as v
 
 from config import API_HASH, API_ID, BOT_TOKEN, LOG_GROUP_ID, STRING_SESSION
 from Vivek.functions.client import VClient  # This is pyrogram modified Client
 
-from .functions import dir, edit_filters
+from .functions import edit_filters
 from .functions.logger import LOGGER  # Logger
-
-dir()  # remove all media files
 
 edit_filters()  # Editing Some Pyrogram Filters
 
@@ -32,3 +34,23 @@ app.bot = VClient(
     in_memory=True,
     max_concurrent_transmissions=9,
 )
+
+files = [
+    ".jpg",
+    ".jpeg",
+    ".mp3",
+    ".m4a",
+    ".mp4",
+    ".webm",
+    ".png",
+    ".session",
+    ".session-journal",
+]
+
+for file in os.listdir():
+    if any(file.endswith(ext) for ext in files):
+        os.remove(file)
+
+if "downloads" in os.listdir():
+    shutil.rmtree("downloads")
+    os.mkdir("downloads")
