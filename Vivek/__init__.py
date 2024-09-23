c80fa75f787aeafd9d4d820fd8d32010f9cabca0
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-23 15:33:47 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -1,10 +1,32 @@
 import uvloop
 
+# Applying uvloop event loop policy
+
 uvloop.install()
-from .core.client import app
-from .core.logger import LOGGER
-from .functions.dir import dir
-from .functions.filters import edit_filters
+
+# Logger 
+from .functions.logger import LOGGER
+
+# functions for editing pyrogram filters or for removing media's on starting
+
+from .functions import edit_filters, dir
+
+import asyncio
+import importlib
+from typing import Awaitable, Callable
+
+import pyromod.listen  # noqa
+from pyrogram import __version__ as v #noqa
+from pyrogram import idle #noqa
+
+from config import API_HASH, API_ID, BOT_TOKEN, LOG_GROUP_ID, STRING_SESSION #noqa
+from Vivek.functions.client import VClient
+from Vivek.plugins import ALL_MODULES #noqa
+
+from .logger import LOGGER
+
 
 dir()
 edit_filters()
+
+app = 
