b369e79ff4539edade4d5bf542594e14af3ddc75
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-23 15:37:45 +0530

    Update __init__.py

--- Vivek/__init__.py
+++ Vivek/__init__.py
@@ -11,22 +11,33 @@ from .functions.logger import LOGGER
 
 from .functions import edit_filters, dir
 
-import asyncio
-import importlib
-from typing import Awaitable, Callable
-
-import pyromod.listen  # noqa
-from pyrogram import __version__ as v #noqa
-from pyrogram import idle #noqa
-
-from config import API_HASH, API_ID, BOT_TOKEN, LOG_GROUP_ID, STRING_SESSION #noqa
-from Vivek.functions.client import VClient
-from Vivek.plugins import ALL_MODULES #noqa
-
-from .logger import LOGGER
+from Vivek.functions.client import VClient # This is pyrogram modified Client 
 
 
 dir()
 edit_filters()
 
-app = 
+import pyromod.listen  # noqa
+from pyrogram import __version__ as v
+
+from config import API_HASH, API_ID, BOT_TOKEN, LOG_GROUP_ID, STRING_SESSION
+
+
+app = Vclient(
+    name="Vivek",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    app_version=f"Cute {v}",
+    session_string=STRING_SESSION,
+    in_memory=True,
+)
+
+app.bot = VClient(
+    name="Vivek1",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    app_version=f"Cute {v}",
+    bot_token=BOT_TOKEN,
+    in_memory=True,
+    max_concurrent_transmissions=9,
+)
