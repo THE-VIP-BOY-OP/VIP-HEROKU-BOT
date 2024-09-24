369cec09f91a7347ead24b30505f1e829cd141fe
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-24 19:37:16 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -12,7 +12,7 @@ async def main():
     await app.start()  # Starting Userbot client
     LOGGER(__name__).info(f"Userbot started")
     await app.bot.start()  # Starting bot client
-    await app.bot.send_message(LOG_GROUP_ID, "started")
+    await app.bot.send_message(app.me.id, "started") # sending message that bot is started to Userbot 
     LOGGER(__name__).info(f"bot started")
     for all_module in ALL_MODULES:
         importlib.import_module("Vivek.plugins" + all_module)
