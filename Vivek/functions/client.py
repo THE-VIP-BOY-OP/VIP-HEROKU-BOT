facc6c4c87fcb99d1e41ab30cad55cf29f4c9750
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-24 20:31:44 +0530

    Update client.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- Vivek/functions/client.py
+++ Vivek/functions/client.py
@@ -10,14 +10,6 @@ class VClient(Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.help = BotHelp
-        self.id = self.me.id
-        self.username = self.me.username
-        self.mention = self.me.mention
-        self.first_name = self.me.first_name
-        self.last_name = self.me.last_name
-        self.name = (
-            self.first_name + self.last_name if self.last_name else self.first_name
-        )
 
     async def restart_script(self):
         os.execvp(sys.executable, [sys.executable, "-m", "Vivek", *sys.argv[1:]])
