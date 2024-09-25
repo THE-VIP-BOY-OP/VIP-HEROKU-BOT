910c2a47c07a65cfec55600c6c0f41a24e6df88a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-25 13:33:36 +0530

    Update modules.py

--- Vivek/plugins/tools/modules.py
+++ Vivek/plugins/tools/modules.py
@@ -8,13 +8,13 @@ from Vivek import FUNCTIONS, app
 @app.on_message(filters.command("listloaded") & filters.sudo)
 async def list_all_modules(client, message):
     all_funcs = FUNCTIONS["MODULES"]
-    this_func = sys._getframe().f_code
-    if this_func in all_funcs:
-        all_funcs.remove(this_func)
     msg = await message.reply_text("Getting all loaded modules function")
     count = 0
     text = ""
     for func in all_funcs:
+        if func.__name__ == sys._getframe().f_code.co_name:
+            func.remove(sys._getframe().f_code.co_name)
+            continue
         count += 1
         text += f"{count} `{func.__name__}`\n"
     if text != "":
