bffbfdbc63ddfe09dc29e28144c9fa985c2f2426
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-25 13:15:28 +0530

    Update modules.py

--- Vivek/plugins/tools/modules.py
+++ Vivek/plugins/tools/modules.py
@@ -11,11 +11,11 @@ async def list_all_modules(client, message):
     this_func = sys._getframe().f_code
     if this_func in all_funcs:
         all_funcs.remove(this_func)
-    await message.reply_text("Getting all loaded modules function")
+    msg = await message.reply_text("Getting all loaded modules function")
     count = 0
     text = ""
     for func in all_funcs:
         count += 1
         text += f"{count} `{func.__name__}`\n"
     if text != "":
-        await message.reply_text(text)
+        await msg.edit(text)
