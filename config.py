d962569d790447f88f5309d9782233a0780994a9
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-25 17:07:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,6 @@ API_HASH = getenv("API_HASH", "")
 
 STRING_SESSION = getenv("STRING_SESSION", None)
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-
 DATABASE_CHANNEL_ID = getenv("DATABASE_CHANNEL_ID", -1002351221659)
 
 LOG_GROUP_ID = getenv("LOG_GROUP_ID", -1002146211959)
@@ -24,10 +22,8 @@ OWNER_ID = list(
     map(int, getenv("OWNER_ID", "6815918609 7301077117 7428672286 7157974692").split())
 )
 
-PREFIX = list(map(str, getenv("PREFIX", "!").split()))
+PREFIX = list(map(str, getenv("PREFIX", "! .").split()))
 
-
-ONLY_FOR_SUDO = bool(getenv("ONLY_FOR_SUDO", True))
 
 GIT_TOKEN = getenv("GIT_TOKEN", "")
 
