336fe64ab7c8c7458982f6b284a85d5752b2c19c
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-25 11:43:30 +0530

    Update __main__.py

--- Vivek/__main__.py
+++ Vivek/__main__.py
@@ -20,9 +20,13 @@ async def main():
                 modules[imported_module.__mod__.lower()] = (
                     imported_module  # storing the impported module in 'modules' dictionary
                 )
-        if hasattr(imported_module, "__handlers__") and imported_module.__handlers__:
-            for function in imported_module.__handlers__:
-                FUNCTIONS.append(function)
+        functions = [func for func in dir(imported_module) if callable(getattr(imported_module, func))]
+        handlers = [
+            func for func in functions if hasattr(getattr(imported_module, func), 'handlers')
+        ]
+        for func in functions_with_handlers:
+            FUNCTIONS.append(func)
+
     await idle()  # Run this bot without stopping
     # Stop the app and bot if keyboard interrupt (CTRL + C PRESSED)
     await app.stop()
