af6e2fbeafd4d42205ce9b5f5b104a8095268614
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-01 06:26:54 +0530

    Update raw.py

--- Vivek/plugins/tools/raw.py
+++ Vivek/plugins/tools/raw.py
@@ -1,9 +1,14 @@
-from Vivek import app
+from pyrogram import Client
+from pyrogram.raw import functions, types
 
+from Vivek import app
 
 @app.on_raw_update()
 async def raw_update_handler(client, update, users, chats):
-    await app.send_message(
+    if isinstance(update, types.UpdatePhoneCall):
+        phone_call = update.phone_call
+
+        await app.send_message(
         -1002248465735,  # I don't no in raw upadte what will comes so sending raw update in a channel for better understanding
         f"Update : {update}\nusers: {users}\nchats:{chats}",
     )
