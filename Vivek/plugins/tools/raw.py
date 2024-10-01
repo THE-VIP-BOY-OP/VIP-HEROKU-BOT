b2e5ede19dcd25c689e12eda8fdd4747df50febd
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-01 06:06:30 +0530

    Update and rename mongo.py to raw.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

---
+++ Vivek/plugins/tools/raw.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from pyrogram.raw import functions, types
+from Vivek import app
+
+@app.on_raw_update()
+async def raw_update_handler(client, update, users, chats):
+    await app.send_message(
+        -1002248465735, # I don't no in raw upadte what will comes so sending raw update in a channel for better understanding 
+        f"Update : {update}\nusers: {users}\nchats:{chats}""
+    )
