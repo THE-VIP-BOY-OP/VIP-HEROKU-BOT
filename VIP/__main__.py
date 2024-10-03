3aeb15a3d4ef09270058480f403081da524d692a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-03 13:54:30 +0530

    Update __main__.py

--- VIP/__main__.py
+++ VIP/__main__.py
@@ -7,20 +7,25 @@ from pyrogram import idle
 from VIP import app
 from VIP.plugins import ALL_MODULES
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()]  # Output to console
+)
+
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pymongo").setLevel(logging.ERROR)
 
 log = logging.getLogger("VIP-HEROKU-BOT")
 
 async def main():
-    log.info("VIP-HEROKU-BOT:  Starting bot...")
+    log.info("Starting bot...")
     await app.start()
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module("VIP.plugins" + all_module)
-    log.info('VIP-HEROKU-BOT:  Bot Started')
+        imported_module = importlib.import_module("VIP.plugins." + all_module)
+    log.info("Bot Started")
     await idle()
     await app.stop()
-
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop_policy().get_event_loop()
