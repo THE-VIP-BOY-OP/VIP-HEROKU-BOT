c1e7e64ca17859a9fdd5fac01ce9a32efb4fa0e4
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-03 13:30:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,8 @@ API_HASH = getenv("API_HASH", "")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 
-SUDOERS = list(
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
+
+OWNER_ID = list(
     map(int, getenv("OWNER_ID", "6815918609 7301077117 7428672286 7157974692").split())
 )
