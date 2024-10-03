f79721120f5ae0a98021de6bf4f6e465bf993eac
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-03 13:10:46 +0530

    Update __init__.py

--- VIP/__init__.py
+++ VIP/__init__.py
@@ -1,45 +1,10 @@
-import uvloop
+from pyrogram import Client
+from config import *
 
-uvloop.install()  # Applying uvloop event loop policy
-
-import os
-import shutil
-
-from pyrogram import __version__ as v
-
-from config import API_HASH, API_ID, LOG_GROUP_ID, STRING_SESSION
-from VIP.functions.client import VClient  # This is pyrogram modified Client
-
-from .functions import ModuleHelp, edit_filters
-from .functions.logger import LOGGER  # Logger
-
-edit_filters()  # Editing Some Pyrogram Filters
-FUNCTIONS = {}  # All functions of commamds that stored here
 app = VClient(
     name="VIP",
     api_id=API_ID,
     api_hash=API_HASH,
-    app_version=f"Cute {v}",
-    session_string=STRING_SESSION,
+    bot_token=BOT_TOKEN,
     in_memory=True,
 )
-
-files = [
-    ".jpg",
-    ".jpeg",
-    ".mp3",
-    ".m4a",
-    ".mp4",
-    ".webm",
-    ".png",
-    ".session",
-    ".session-journal",
-]
-
-for file in os.listdir():
-    if any(file.endswith(ext) for ext in files):
-        os.remove(file)
-
-if "downloads" in os.listdir():
-    shutil.rmtree("downloads")
-    os.mkdir("downloads")
