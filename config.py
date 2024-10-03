727d9d85dc997b3b05ca7b227e57a34a4011b4b1
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-03 13:16:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,21 +12,8 @@ API_ID = int(
 
 API_HASH = getenv("API_HASH", "")
 
-STRING_SESSION = getenv("STRING_SESSION", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
 
-DATABASE_CHANNEL_ID = getenv("DATABASE_CHANNEL_ID", -1002351221659)
-
-LOG_GROUP_ID = getenv("LOG_GROUP_ID", -1002146211959)
-
-OWNER_ID = list(
+SUDOERS = list(
     map(int, getenv("OWNER_ID", "6815918609 7301077117 7428672286 7157974692").split())
 )
-
-PREFIX = list(map(str, getenv("PREFIX", "! .").split()))
-
-
-GIT_TOKEN = getenv("GIT_TOKEN", "")
-
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Vivekkumar-IN/Vivek")
