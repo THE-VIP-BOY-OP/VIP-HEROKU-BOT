41d0a236e44202e64db7c3d36109565746288a3d
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-03 13:48:02 +0530

    Update callback.py

--- VIP/plugins/heroku/callback.py
+++ VIP/plugins/heroku/callback.py
@@ -26,6 +26,27 @@ API_HASH = os.getenv("API_HASH")
 
 import re
 
+BASE = "https://batbin.me/"
+
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
+
+async def VIPbin(text):
+    resp = await post(f"{BASE}api/v2/paste", data=text)
+    if not resp["success"]:
+        return
+    link = BASE + resp["message"]
+    return link
+
+
 
 def convert_to_small_caps(text):
     # Mapping for regular letters to small caps
